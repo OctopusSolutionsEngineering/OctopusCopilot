358566e1d72c3e92624b49ea37e2ddbe634087fe
Matthew Casperson <matthewcasperson@gmail.com>
2024-02-10 15:56:03 +1000

    Automated tests

--- main.py
+++ main.py
@@ -1,58 +1,56 @@
 from datetime import datetime
 from langchain_experimental.llms.ollama_functions import OllamaFunctions
 
+for model_name in ["phi", "mistral", "llama2", "orca2"]:
+    model = OllamaFunctions(model=model_name)
 
-#model = OllamaFunctions(model="phi") #19.7s
-#model = OllamaFunctions(model="mistral") #6.4s
-#model = OllamaFunctions(model="llama2") #8.1s
-model = OllamaFunctions(model="orca2") #6.5s
-#model = OllamaFunctions(model="orca-mini") #fails
-
-model = model.bind(
-    functions=[
-        {
-            "name": "get_current_weather",
-            "description": "Get the current weather in a given location",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "location": {
-                        "type": "string",
-                        "description": "The city and state, " "e.g. San Francisco, CA",
+    model = model.bind(
+        functions=[
+            {
+                "name": "get_current_weather",
+                "description": "Get the current weather in a given location",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "location": {
+                            "type": "string",
+                            "description": "The city and state, " "e.g. San Francisco, CA",
+                        },
+                        "unit": {
+                            "type": "string",
+                            "enum": ["celsius", "fahrenheit"],
+                        },
                     },
-                    "unit": {
-                        "type": "string",
-                        "enum": ["celsius", "fahrenheit"],
-                    },
+                    "required": ["location"],
                 },
-                "required": ["location"],
             },
-        },
-{
-            "name": "find_user",
-            "description": "Rerturn the details of a user",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "name": {
-                        "type": "string",
-                        "description": "The city and state, " "e.g. San Francisco, CA",
+    {
+                "name": "find_user",
+                "description": "Rerturn the details of a user",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "name": {
+                            "type": "string",
+                            "description": "The city and state, " "e.g. San Francisco, CA",
+                        },
+                        "city": {
+                            "type": "string",
+                            "description": "The location of the user"
+                        },
                     },
-                    "city": {
-                        "type": "string",
-                        "description": "The location of the user"
-                    },
+                    "required": ["name"],
                 },
-                "required": ["name"],
-            },
-        }
-    ],
-    #function_call={"name": "get_current_weather"},
-)
+            }
+        ],
+        #function_call={"name": "get_current_weather"},
+    )
 
-start = datetime.now()
-output = model.invoke("what is the weather in Boston?")
-end = datetime.now()
+    model.invoke("what is the weather in Boston?")
+    model.invoke("what is the weather in Boston?")
 
-print(output)
-print(end - start)
+    start = datetime.now()
+    output = model.invoke("what is the weather in Boston?")
+    end = datetime.now()
+
+    print(model_name + " " + str(end - start))
