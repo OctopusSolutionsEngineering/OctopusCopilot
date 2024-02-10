6d9a9078ab7c1930d23d2f8f43d7606e892fcca4
Matthew Casperson <matthewcasperson@gmail.com>
2024-02-10 16:47:42 +1000

    Testing new functions

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
 from datetime import datetime
 from langchain_experimental.llms.ollama_functions import OllamaFunctions
 
-model = OllamaFunctions(model="phi")
+model = OllamaFunctions(model="llama2")
 
 model = model.bind(
     functions=[
         {
             "name": "find_user",
-            "description": "Rerturn the details of a user",
+            "description": "Return the details of a person",
             "parameters": {
                 "type": "object",
                 "properties": {
                     "name": {
                         "type": "string",
-                        "description": "The city and state, " "e.g. San Francisco, CA",
+                        "description": "The name of the person, " "e.g. John or Jessica"
                     },
                     "city": {
                         "type": "string",
-                        "description": "The location of the user"
+                        "description": "The location of the person, " "e.g. San Francisco, CA",
                     },
                 },
                 "required": ["name"],
+            },
+        },
+{
+            "name": "get_deployments",
+            "description": "Return the details of a deployment",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "release_version": {
+                        "type": "string",
+                        "description": "The release version associated with the deployment, " "e.g. 1.2.3 or 0.0.1"
+                    },
+                    "environment": {
+                        "type": "string",
+                        "description": "The name of the environment the release was deployed to, " "e.g. Development, Test, Production",
+                    },
+                },
+                "required": ["release_version", "environment"],
             },
         },
         {
@@ -48,7 +66,7 @@ model = model.bind(
 
 try:
     start = datetime.now()
-    output = model.invoke("what is the weather in Boston?")
+    output = model.invoke("return the details of release 1.4.5 to the production environment")
     end = datetime.now()
 
     print(output)
