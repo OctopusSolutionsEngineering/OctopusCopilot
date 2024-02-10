89dae9560689766943d757a2fb536031c51800b6
Matthew Casperson <matthewcasperson@gmail.com>
2024-02-10 16:16:22 +1000

    Settle on phi as it is the smallest model

--- main.py
+++ main.py
@@ -1,59 +1,59 @@
 from datetime import datetime
 from langchain_experimental.llms.ollama_functions import OllamaFunctions
 
-for model_name in ["phi", "mistral", "llama2", "orca2"]:
-    model = OllamaFunctions(model=model_name)
 
-    model = model.bind(
-        functions=[
-            {
-                "name": "get_current_weather",
-                "description": "Get the current weather in a given location",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "location": {
-                            "type": "string",
-                            "description": "The city and state, " "e.g. San Francisco, CA",
-                        },
-                        "unit": {
-                            "type": "string",
-                            "enum": ["celsius", "fahrenheit"],
-                        },
+model = OllamaFunctions(model="phi")
+
+model = model.bind(
+    functions=[
+        {
+            "name": "get_current_weather",
+            "description": "Get the current weather in a given location",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "The city and state, " "e.g. San Francisco, CA",
                     },
-                    "required": ["location"],
+                    "unit": {
+                        "type": "string",
+                        "enum": ["celsius", "fahrenheit"],
+                    },
                 },
+                "required": ["location"],
             },
-    {
-                "name": "find_user",
-                "description": "Rerturn the details of a user",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "name": {
-                            "type": "string",
-                            "description": "The city and state, " "e.g. San Francisco, CA",
-                        },
-                        "city": {
-                            "type": "string",
-                            "description": "The location of the user"
-                        },
+        },
+{
+            "name": "find_user",
+            "description": "Rerturn the details of a user",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "name": {
+                        "type": "string",
+                        "description": "The city and state, " "e.g. San Francisco, CA",
                     },
-                    "required": ["name"],
+                    "city": {
+                        "type": "string",
+                        "description": "The location of the user"
+                    },
                 },
-            }
-        ],
-        #function_call={"name": "get_current_weather"},
-    )
+                "required": ["name"],
+            },
+        }
+    ],
+    #function_call={"name": "get_current_weather"},
+)
 
-    try:
-        model.invoke("what is the weather in Boston?")
-        model.invoke("what is the weather in Boston?")
+try:
+    start = datetime.now()
+    output = model.invoke("what is the weather in Boston?")
+    end = datetime.now()
 
-        start = datetime.now()
-        output = model.invoke("what is the weather in Boston?")
-        end = datetime.now()
-
-        print(model_name + " " + str(end - start))
-    except Exception as e:
-        print(e)
+    print(output)
+    print(output.additional_kwargs['function_call']['name'])
+    print(output.additional_kwargs['function_call']['arguments'])
+    print(str(end - start))
+except Exception as e:
+    print(e)
