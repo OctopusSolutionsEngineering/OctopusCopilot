d956ee194fc66609d9336f742f3391349bd99cee
Matthew Casperson <matthewcasperson@gmail.com>
2024-02-14 10:09:33 +1000

    Function calling now works

--- domain/copilot_handler.py
+++ domain/copilot_handler.py
@@ -1,19 +1,30 @@
 import os
 
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import AzureChatOpenAI
+from langchain.agents import OpenAIFunctionsAgent
+from langchain_core.tools import StructuredTool
+from domain.azure_chat_open_ai_with_tooling import AzureChatOpenAIWithTooling
+from infrastructure.octopus_projects import get_octopus_projects
 
 
 def handle_copilot_chat(query):
-    prompt = ChatPromptTemplate.from_template(query)
-    output_parser = StrOutputParser()
-    model = AzureChatOpenAI(temperature=0,
-                            azure_deployment="OctopusCopilotFunctionCalling",
-                            openai_api_key=os.environ["OPENAI_API_KEY"],
-                            azure_endpoint=os.environ["OPENAI_ENDPOINT"],
-                            api_version="2023-05-15")
+    tools = build_tools()
+    # Version comes from https://github.com/openai/openai-python/issues/926#issuecomment-1839426482
+    # Note that for function calling you need 3.5-turbo-16k
+    # https://github.com/openai/openai-python/issues/926#issuecomment-1920037903
+    agent = OpenAIFunctionsAgent.from_llm_and_tools(
+        llm=AzureChatOpenAIWithTooling(temperature=0,
+                                       azure_deployment="OctopusCopilotFunctionCalling2",
+                                       openai_api_key=os.environ["OPENAI_API_KEY"],
+                                       azure_endpoint=os.environ["OPENAI_ENDPOINT"],
+                                       api_version="2023-12-01-preview"),
+        tools=tools,
+    )
 
-    chain = prompt | model | output_parser
-    output = chain.invoke({})
-    print(output)
+    action = agent.plan([], input=query)
+    print(action)
+
+    print(globals()[action.tool](**action.tool_input))
+
+
+def build_tools():
+    return [StructuredTool.from_function(get_octopus_projects)]
---
+++ infrastructure/octopus_projects.py
@@ -0,0 +1,7 @@
+def get_octopus_projects(space_name):
+    """Return a list of projects in an Octopus space
+
+        Args:
+            space_name: The name of the space containing the projects
+    """
+    return ["Project1", "Project2"]
