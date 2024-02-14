85b55326b27e0d4870ec638ba285b4ea3e727372
Matthew Casperson <matthewcasperson@gmail.com>
2024-02-14 16:08:55 +1000

    Added more tests

--- tests/mock/mock_projects_test.py
+++ tests/mock/mock_projects_test.py
@@ -1,16 +1,25 @@
 import unittest
 
+from parameterized import parameterized
+
 from domain.handlers.copilot_handler import handle_copilot_chat
 from tests.infrastructure.tools.build_test_tools import build_test_tools
 
 
 class MockRequests(unittest.TestCase):
-    def test_get_projects(self):
+
+    @parameterized.expand([
+        "What are the projects associated with space MySpace?",
+        "List the projects saved under MySpace.",
+        "projcets under MySpace.",
+        "Please show me the projects that have been created under the space called MySpace.",
+    ])
+    def test_get_projects(self, query):
         """
         Tests that the llm can find the appropriate mock function and arguments
         """
 
-        function = handle_copilot_chat("What are the projects associated with space MySpace?", build_test_tools)
+        function = handle_copilot_chat(query, build_test_tools)
 
         self.assertEqual(function.function.__name__, "get_octopus_projects")
         self.assertEqual(function.function_args["space_name"], "MySpace")
