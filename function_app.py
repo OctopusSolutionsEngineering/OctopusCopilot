766c78e836eabb7ff4449d84d2f6a0b1ec802599
Matthew Casperson <matthewcasperson@gmail.com>
2024-02-19 11:45:28 +1000

    pass query as param

--- function_app.py
+++ function_app.py
@@ -64,7 +64,7 @@ def query_form_handler(req: func.HttpRequest) -> func.HttpResponse:
 
 
 @app.route(route="copilot_handler", auth_level=func.AuthLevel.ANONYMOUS)
-def copilot_handler(req: func.HttpRequest) -> func.HttpResponse:
+def copilot_handler(req: func.HttpRequest, urlparse=None) -> func.HttpResponse:
     """
     A function handler that processes a plain text query
     :param req: The HTTP request
@@ -102,8 +102,7 @@ def copilot_handler(req: func.HttpRequest) -> func.HttpResponse:
     }
 
     try:
-        query = "\n".join(map(lambda m: m["content"], req_body["messages"]))
-        result = handle_copilot_chat(query, build_form_tools).call_function()
+        result = handle_copilot_chat(req.params.get("message"), build_form_tools).call_function()
 
         # Create data only SSE response
         # https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#data-only_messages
