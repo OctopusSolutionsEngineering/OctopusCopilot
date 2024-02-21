ae47604331efbd3becc9a08bd5ea192b43e37e42
Matthew Casperson <matthewcasperson@gmail.com>
2024-02-21 12:55:37 +1000

    Don't force an algorithm

--- domain/jwt/oidc.py
+++ domain/jwt/oidc.py
@@ -4,4 +4,4 @@ instance = JWT()
 
 
 def parse_jwt(token):
-    return instance.decode(message=token, do_time_check=True, algorithms={"HS256"})
+    return instance.decode(message=token, do_time_check=True)
