af3cf7a86db001184e28d668ff49336844acc821
Matthew Casperson <matthewcasperson@gmail.com>
2024-02-23 11:13:52 +1000

    Added comments

--- function_app.py
+++ function_app.py
@@ -211,6 +211,15 @@ def copilot_handler(req: func.HttpRequest) -> func.HttpResponse:
 
 
 def extract_query(req: func.HttpRequest):
+    """
+    Traditional web based SSE only supports GET requests, so testing the integration from a HTML form
+    means making a GET request with the query as a parameter. Copilot makes POST requests with the
+    query in the body. This functon extracts the query for both HTML forms and Copilot requests.
+    :param req: The HTTP request
+    :return: The query
+    """
+
+    # This is the query from a HTML form
     query = req.params.get("message")
 
     if query:
