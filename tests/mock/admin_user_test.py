40e0fb015d78d1544c5c24473e770ee490185241
Matthew Casperson <matthewcasperson@gmail.com>
2024-02-23 10:17:06 +1000

    Added more tests

--- tests/mock/admin_user_test.py
+++ tests/mock/admin_user_test.py
@@ -1,8 +1,25 @@
 import unittest
 
+from domain.exceptions.not_authorized import NotAuthorized
 from domain.security.security import is_admin_user
 
 
 class AdminUser(unittest.TestCase):
     def test_is_admin_user(self):
         is_admin_user(lambda: 123, lambda: "[123]", lambda: print("success"))
+
+    def test_is_admin_user_array_string(self):
+        is_admin_user(lambda: 123, lambda: "[\"123\"]", lambda: print("success"))
+
+    def test_is_admin_user_string(self):
+        is_admin_user(lambda: '123', lambda: "[123]", lambda: print("success"))
+
+    def test_is_admin_user_both_strings(self):
+        is_admin_user(lambda: '123', lambda: "[\"123\"]", lambda: print("success"))
+
+    def test_is_admin_user_not_found(self):
+        try:
+            is_admin_user(lambda: '1233', lambda: "[\"123\"]", lambda: print("success"))
+            self.fail()
+        except NotAuthorized as e:
+            pass
