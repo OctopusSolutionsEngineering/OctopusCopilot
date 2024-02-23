c27ef3548ed0377ea7240cf59efb62875abefa08
Matthew Casperson <matthewcasperson@gmail.com>
2024-02-23 11:34:37 +1000

    Log any failed slack requests

--- infrastructure/slack.py
+++ infrastructure/slack.py
@@ -15,7 +15,10 @@ def send_slack_message(message, get_slack_url):
     """
     try:
         data = json.dumps({"text": message})
-        http.request("POST", get_slack_url(), headers={'Content-Type': 'application/json'}, data=data)
+        resp = http.request("POST", get_slack_url(), headers={'Content-Type': 'application/json'}, data=data)
+
+        if resp.status != 200:
+            logger.error(resp.data)
     except Exception as e:
         error_message = getattr(e, 'message', repr(e))
         logger.error(error_message)
