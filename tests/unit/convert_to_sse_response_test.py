d31130699162cdbb93a1670a435a91b572aa86a3
Matthew Casperson <matthewcasperson@gmail.com>
2024-02-27 10:55:09 +1000

    Added some tests

---
+++ tests/unit/convert_to_sse_response_test.py
@@ -0,0 +1,10 @@
+import json
+import unittest
+
+from domain.transformers.sse_transformers import convert_to_sse_response
+
+
+class ConvertToSseResponse(unittest.TestCase):
+    def test_convert_to_sse_response(self):
+        self.assertEqual(json.loads(convert_to_sse_response("hi").replace("data:", "")).get('content'), "hi")
+        self.assertEqual(json.loads(convert_to_sse_response("hi").replace("data:", "")).get('role'), "assistant")
