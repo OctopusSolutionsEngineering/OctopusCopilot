32f8474cc45a5db38169d8f672d239f4e1bea67a
Matthew Casperson <matthewcasperson@gmail.com>
2024-02-27 12:30:05 +1000

    Added more tests

--- tests/unit/encryption_test.py
+++ tests/unit/encryption_test.py
@@ -11,3 +11,11 @@ class EncryptionTests(unittest.TestCase):
         decrypted = decrypt_eax(password, ciphered_data, tag, nonce, "salt")
 
         self.assertEqual(decrypted.decode(), text)
+
+    def test_bad_key(self):
+        with self.assertRaises(ValueError):
+            text = "This is a test"
+            password = generate_password("password", "salt")
+            password2 = generate_password("password2", "salt")
+            ciphered_data, tag, nonce = encrypt_eax(text, password, "salt")
+            decrypt_eax(password2, ciphered_data, tag, nonce, "salt")
