8f0d26d9162fb3053248fc4b868e542d9d4269ed
Matthew Casperson <matthewcasperson@gmail.com>
2024-02-27 10:20:56 +1000

    Send back hte JSON blobs

--- domain/transformers/sse_transformers.py
+++ domain/transformers/sse_transformers.py
@@ -1,3 +1,6 @@
+import json
+
+
 def convert_to_sse_response(result):
     """
     Converts a string to an SSE data only response.
@@ -10,4 +13,4 @@ def convert_to_sse_response(result):
 
     # https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#data-only_messages
     # "Each notification is sent as a block of text terminated by a pair of newlines."
-    return "\n".join(map(lambda l: "data: " + l, result.split("\n"))) + "\n\n"
+    return "\n".join(map(lambda l: "data: " + l, json.dumps({"content": result.split("\n"), "timestamp": 0}))) + "\n\n"
