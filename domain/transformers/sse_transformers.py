5bef3787f19196e6a66d57441e9cc320ce651739
Matthew Casperson <matthewcasperson@gmail.com>
2024-02-27 19:20:28 +1000

    Filter API keys from the output

--- domain/transformers/sse_transformers.py
+++ domain/transformers/sse_transformers.py
@@ -1,5 +1,7 @@
 import json
 
+from domain.errors.error_handling import sanitize_message
+
 
 def convert_to_sse_response(result):
     """
@@ -16,7 +18,8 @@ def convert_to_sse_response(result):
     # https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#data-only_messages
     # "Each notification is sent as a block of text terminated by a pair of newlines."
     content = "\n".join(
-        map(lambda l: "data: " + json.dumps({"choices": [{"index": 0, "delta": {"content": l + "\n"}}]}),
+        map(lambda l: "data: " + json.dumps(
+            {"choices": [{"index": 0, "delta": {"content": sanitize_message(l) + "\n"}}]}),
             result.strip().split("\n")))
 
     return content + "\n" + stop + "\n\n"
