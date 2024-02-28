e421ddddf350e9751ffda217ec4d0589dc35c617
Matthew Casperson <matthewcasperson@gmail.com>
2024-02-28 13:06:04 +1000

    Fixed linting

--- tests/live/users_test.py
+++ tests/live/users_test.py
@@ -43,6 +43,6 @@ class UsersTest(unittest.TestCase):
         self.assertEqual(0, delete_old_user_details(connection_string))
 
     def valid_uuid(self, uuid):
-        regex = re.compile('^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}\Z', re.I)
+        regex = re.compile('^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}', re.I)
         match = regex.match(uuid)
         return bool(match)
