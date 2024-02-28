1b39c7f03f4a0433dfab771d89bf619c7de8a246
Matthew Casperson <matthewcasperson@gmail.com>
2024-02-28 13:05:30 +1000

    Added more tests of tables interactions

--- tests/live/users_test.py
+++ tests/live/users_test.py
@@ -2,7 +2,8 @@ import re
 import unittest
 
 from infrastructure.users import save_default_values, get_default_values, save_login_uuid, delete_login_uuid, \
-    save_users_octopus_url_from_login, get_users_details
+    save_users_octopus_url_from_login, get_users_details, delete_all_user_details, delete_old_user_details, \
+    delete_old_user_login_records
 
 connection_string = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
 
@@ -22,6 +23,8 @@ class UsersTest(unittest.TestCase):
         with self.assertRaises(Exception):
             save_users_octopus_url_from_login(uuid, "https://test.com", "apikey", "password", "salt", connection_string)
 
+        self.assertEqual(0, delete_old_user_login_records(connection_string))
+
     def test_login(self):
         uuid = save_login_uuid("test", connection_string)
         save_users_octopus_url_from_login(uuid, "https://test.com", "apikey", "password", "salt", connection_string)
@@ -31,6 +34,13 @@ class UsersTest(unittest.TestCase):
         self.assertTrue(user['OctopusApiKey'])
         self.assertTrue(user['EncryptionTag'])
         self.assertTrue(user['EncryptionNonce'])
+
+        delete_all_user_details(connection_string)
+
+        with self.assertRaises(Exception):
+            get_users_details("test", connection_string)
+
+        self.assertEqual(0, delete_old_user_details(connection_string))
 
     def valid_uuid(self, uuid):
         regex = re.compile('^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}\Z', re.I)
