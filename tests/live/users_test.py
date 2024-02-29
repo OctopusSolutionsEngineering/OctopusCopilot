58e7f68063fcb99ae4dba2031e0d50d9395f987e
Matthew Casperson <matthewcasperson@gmail.com>
2024-03-01 07:16:56 +1000

    Fixed tests

--- tests/live/users_test.py
+++ tests/live/users_test.py
@@ -1,9 +1,8 @@
 import re
 import unittest
 
-from infrastructure.users import save_default_values, get_default_values, save_login_uuid, delete_login_uuid, \
-    save_users_octopus_url_from_login, get_users_details, delete_all_user_details, delete_old_user_details, \
-    delete_old_user_login_records
+from infrastructure.users import save_default_values, get_default_values, \
+    save_users_octopus_url_from_login, get_users_details, delete_all_user_details, delete_old_user_details
 
 connection_string = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
 
@@ -20,19 +19,8 @@ class UsersTest(unittest.TestCase):
 
         self.assertEqual("Default", value)
 
-    def test_save_uuid(self):
-        uuid = save_login_uuid("test", connection_string)
-        self.assertTrue(self.valid_uuid(uuid))
-        delete_login_uuid(uuid, connection_string)
-
-        with self.assertRaises(Exception):
-            save_users_octopus_url_from_login(uuid, "https://test.com", "apikey", "password", "salt", connection_string)
-
-        self.assertEqual(0, delete_old_user_login_records(connection_string))
-
     def test_login(self):
-        uuid = save_login_uuid("test", connection_string)
-        save_users_octopus_url_from_login(uuid, "https://test.com", "apikey", "password", "salt", connection_string)
+        save_users_octopus_url_from_login("12345", "https://test.com", "apikey", "password", "salt", connection_string)
         user = get_users_details("test", connection_string)
 
         self.assertTrue(user['OctopusUrl'])
