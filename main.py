219dd1380331c7863e9184f20e167ef1e34d17e2
Matthew Casperson <matthewcasperson@gmail.com>
2024-03-06 10:26:06 +1000

    Restored the previous CLI tool

--- main.py
+++ main.py
@@ -94,6 +94,7 @@ def build_tools():
     """
     return FunctionDefinitions([
         FunctionDefinition(answer_general_query),
+        FunctionDefinition(get_octopus_project_names_cli),
     ])
 
 
