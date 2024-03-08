f36f46b4cd27f7c0b46f73fbc94b8ac0f332f09a
Matthew Casperson <matthewcasperson@gmail.com>
2024-03-08 14:25:15 +1000

    Log to the console too

--- domain/logging/query_loggin.py
+++ domain/logging/query_loggin.py
@@ -1,6 +1,9 @@
 from domain.config.slack import get_slack_url
+from domain.logging.app_logging import configure_logging
 from domain.strings.sanitized_list import sanitize_list
 from infrastructure.slack import send_slack_message_async
+
+logger = configure_logging(__name__)
 
 
 def log_query(message, query):
@@ -10,4 +13,6 @@ def log_query(message, query):
     :param query: The message
     """
     sanitized_query = sanitize_list(query)
-    send_slack_message_async(message + " " + ",".join(sanitized_query), get_slack_url())
+    complete_message = message + " " + ",".join(sanitized_query)
+    send_slack_message_async(complete_message, get_slack_url())
+    logger(complete_message)
