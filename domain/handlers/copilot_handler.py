bae813035b2033423badffdf8974c83f451fa49d
Matthew Casperson <matthewcasperson@gmail.com>
2024-03-13 16:34:01 +1000

    Added comments

--- domain/handlers/copilot_handler.py
+++ domain/handlers/copilot_handler.py
@@ -98,6 +98,27 @@ def build_hcl_and_json_prompt(step_by_step=False):
 def collect_llm_context(query, space_name, project_names, runbook_names, target_names, tenant_names,
                         library_variable_sets, api_key,
                         octopus_url, log_query, step_by_step=False):
+    """
+    We need to source context for the LLM from multiple locations. "Static" resources are defined using Terraform,
+    as this is a publicly documented format the LLM may have had an opportunity to scrape that also has the benefit of
+    explicitly defining the relationship between resources. "Dynamic" resources like deployments and releases are
+    sourced from the Octopus API as these can not be defined in HCL.
+
+    The LLM messages are also tailored here to guide the LLM in how it processes the context.
+
+    :param query: The LLM query
+    :param space_name: The Octopus space name
+    :param project_names: The project names found in the query
+    :param runbook_names: The runbook names found in the query
+    :param target_names: The target names found in the query
+    :param tenant_names: The tenant names found in the query
+    :param library_variable_sets: The library variable set names found in the query
+    :param api_key: The Octopus API key
+    :param octopus_url: The Octopus URL
+    :param log_query: A function used to log debug and error messages
+    :param step_by_step: True if the LLM should be instructed to explain its reasoning
+    :return: The query result
+    """
     ensure_string_not_empty(query, 'query must be a non-empty string (handle_copilot_query).')
     ensure_string_not_empty(space_name, 'space_name must be a non-empty string (handle_copilot_query).')
 
