38507540005bda7b35cbe557a18cd0ac5f55adc5
Matthew Casperson <matthewcasperson@gmail.com>
2024-03-15 07:21:32 +1000

    Added thea bility to parse a bool

--- domain/strings/sanitized_list.py
+++ domain/strings/sanitized_list.py
@@ -67,6 +67,12 @@ def sanitize_library_variable_sets(input_list):
                          "\\*|(Library\\s*)?Variable\\s*Set\\s*[0-9A-Z]|Variables|My\\s*Variable\\s*Set")
 
 
+def sanitize_bool(input_bool):
+    if isinstance(input_bool, bool):
+        return input_bool
+    return False
+
+
 def sanitize_list(input_list, ignored_re=None):
     """
     OpenAI can provide some unexpected inputs. This function cleans them up.
