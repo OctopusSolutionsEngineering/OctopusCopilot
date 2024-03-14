76bbded9d5157777ac0da07bd5ba0671442f0ff2
Matthew Casperson <matthewcasperson@gmail.com>
2024-03-14 11:19:42 +1000

    Fixing up the instructions

--- domain/handlers/copilot_handler.py
+++ domain/handlers/copilot_handler.py
@@ -64,7 +64,7 @@ def build_plain_text_prompt():
     :return: The messages to pass to the llm.
     """
     messages = [
-        ("system", "You understand Terraform modules defining Octopus Deploy log files. "
+        ("system", "You understand Octopus Deploy log files. "
          + "You are a concise and helpful agent. "
          + "Answer the question given the supplied text. You must assume that the supplied text relates to the project and environment in the question."),
         ("user", "{input}"),
