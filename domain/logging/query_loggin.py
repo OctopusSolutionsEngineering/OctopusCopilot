3ccfdb4888234aaa6d601afb5e388ae6cbdb374d
Matthew Casperson <matthewcasperson@gmail.com>
2024-03-16 07:34:38 +1000

    Log to the console before slack

--- domain/logging/query_loggin.py
+++ domain/logging/query_loggin.py
@@ -17,5 +17,5 @@ def log_query(message, query):
 
     sanitized_query = sanitize_list(query)
     complete_message = message + " " + ",".join(sanitized_query)
-    send_slack_message_async(complete_message, get_slack_url())
     logger.info(complete_message)
+    send_slack_message_async(complete_message, get_slack_url())
