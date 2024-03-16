fe44792363570702a3912ab01cb1d1858d5250f8
Matthew Casperson <matthewcasperson@gmail.com>
2024-03-16 19:23:37 +1000

    Try to send the error message

--- domain/handlers/copilot_handler.py
+++ domain/handlers/copilot_handler.py
@@ -173,8 +173,8 @@ def llm_message_query(message_prompt, context, log_query=None):
     except openai.BadRequestError as e:
         # This will be something like:
         # {'error': {'message': "This model's maximum context length is 16384 tokens. However, your messages resulted in 17570 tokens. Please reduce the length of the messages.", 'type': 'invalid_request_error', 'param': 'messages', 'code': 'context_length_exceeded'}}
-        if e.body and e.body.error and e.body.error.message:
-            return e.body.error.message
+        if e.body and 'error' in e.body and 'message' in e.body['error']:
+            return e.body['error']['message']
         return e.message
 
     if log_query:
