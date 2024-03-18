3967e9c87e01858d6b1a70deea62a39f027a0e00
Matthew Casperson <matthewcasperson@gmail.com>
2024-03-18 14:12:07 +1000

    Add another test

--- tests/application/copilot_chat.py
+++ tests/application/copilot_chat.py
@@ -5,6 +5,7 @@ import time
 import unittest
 
 import azure.functions as func
+from retry import retry
 from testcontainers.core.container import DockerContainer
 from testcontainers.core.waiting_utils import wait_for_logs
 
@@ -95,16 +96,14 @@ class CopilotChatTest(unittest.TestCase):
 
         self.assertTrue("Test.Variable" in response_text)
 
+    @retry(AssertionError, tries=3, delay=2)
     def test_get_latest_deployment(self):
         create_and_deploy_release(space_name="Simple")
-
-        time.sleep(30)
-
         prompt = "Get the release version of the latest deployment to the \"Development\" environment for the \"Project1\" project."
         response = copilot_handler_internal(build_request(prompt))
         response_text = response.get_body().decode('utf8')
 
-        self.assertTrue(re.match("0\\.0\\.[1-9]", response_text))
+        self.assertTrue(re.search("0\\.0\\.[1-9]", response_text))
 
     def test_general_question(self):
         prompt = "What does the project \"Project1\" do?"
