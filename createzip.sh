5f6e578d64b1ee23bfa83716369970b4606f5a88
Matthew Casperson <matthewcasperson@gmail.com>
2024-03-24 18:18:50 +1000

    Added a script to create the zip file

---
+++ createzip.sh
@@ -0,0 +1,30 @@
+zip -r OctopusCopilot.0.0.1.zip . \
+  -x "venv/*" \
+  -x ".git/*" \
+  -x ".pytest_cache/*" \
+  -x ".pytest_cache/*" \
+  -x ".idea/*" \
+  -x ".github/*" \
+  -x ".vscode/*" \
+  -x "htmlcov/*" \
+  -x "azure/*" \
+  -x "octopus/*" \
+  -x "jmeter/*" \
+  -x "__pycache__/*" \
+  '*.py' \
+  '*.pyc' \
+  '*.html' \
+  '*.htm' \
+  '*.css' \
+  '*.js' \
+  '*.min' \
+  '*.map' \
+  '*.sql' \
+  '*.png' \
+  '*.jpg' \
+  '*.jpeg' \
+  '*.gif' \
+  '*.json' \
+  '*.env' \
+  '*.txt' \
+  '*.Procfile'
