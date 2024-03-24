3554fc7c5d1c8827064a4df5abf3062946f9b41e
Matthew Casperson <matthewcasperson@gmail.com>
2024-03-24 18:27:03 +1000

    Fixed up zip file

--- createzip.sh
+++ createzip.sh
@@ -2,7 +2,7 @@ zip -r OctopusCopilot.0.0.1.zip . \
   -x "venv/*" \
   -x ".git/*" \
   -x ".pytest_cache/*" \
-  -x ".pytest_cache/*" \
+  -x "**/.pytest_cache/*" \
   -x ".idea/*" \
   -x ".github/*" \
   -x ".vscode/*" \
@@ -11,20 +11,4 @@ zip -r OctopusCopilot.0.0.1.zip . \
   -x "octopus/*" \
   -x "jmeter/*" \
   -x "__pycache__/*" \
-  '*.py' \
-  '*.pyc' \
-  '*.html' \
-  '*.htm' \
-  '*.css' \
-  '*.js' \
-  '*.min' \
-  '*.map' \
-  '*.sql' \
-  '*.png' \
-  '*.jpg' \
-  '*.jpeg' \
-  '*.gif' \
-  '*.json' \
-  '*.env' \
-  '*.txt' \
-  '*.Procfile'
+  -x "**/__pycache__/*"
