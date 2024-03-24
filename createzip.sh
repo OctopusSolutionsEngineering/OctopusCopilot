f5b0ab030b010bd892e9bcc54f29932bcd2deab9
Matthew Casperson <matthewcasperson@gmail.com>
2024-03-24 18:43:18 +1000

    excludes need to be at the end

--- createzip.sh
+++ createzip.sh
@@ -1,14 +1,20 @@
-zip -r OctopusCopilot.0.0.1.zip . \
+zip -R OctopusCopilot.0.0.1.zip \
+  '*.py' \
+  '*.pyc' \
+  '*.html' \
+  '*.htm' \
+  '*.css' \
+  '*.js' \
+  '*.min' \
+  '*.map' \
+  '*.sql' \
+  '*.png' \
+  '*.jpg' \
+  '*.jpeg' \
+  '*.gif' \
+  '*.json' \
+  '*.env' \
+  '*.txt' \
+  '*.Procfile' \
   -x "venv/*" \
-  -x ".git/*" \
-  -x ".pytest_cache/*" \
-  -x "**/.pytest_cache/*" \
-  -x ".idea/*" \
-  -x ".github/*" \
-  -x ".vscode/*" \
   -x "htmlcov/*" \
-  -x "azure/*" \
-  -x "octopus/*" \
-  -x "jmeter/*" \
-  -x "__pycache__/*" \
-  -x "**/__pycache__/*"
