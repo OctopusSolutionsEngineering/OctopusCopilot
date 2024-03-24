6af562d04e81aec8c5f08343e0c6c64e4b8d3431
Matthew Casperson <matthewcasperson@gmail.com>
2024-03-25 06:33:04 +1000

    Make the instructions clearer

--- domain/messages/deployment_logs.py
+++ domain/messages/deployment_logs.py
@@ -4,12 +4,13 @@ def build_plain_text_prompt():
     :return: The messages to pass to the llm.
     """
     messages = [
-        ("system", "You understand Octopus Deploy log files. "
+        ("system", "You understand Octopus Deploy log output. "
          + "You are a concise and helpful agent. "
-         + "Answer the question given the supplied text. You must assume that the supplied text relates to the project and environment in the question."),
+         + "Answer the question given the supplied log output. "
+         + "You must assume that the supplied text relates to the project, environment, tenant, and channel in the question."),
         ("user", "{input}"),
         # https://help.openai.com/en/articles/6654000-best-practices-for-prompt-engineering-with-the-openai-api
         # Put instructions at the beginning of the prompt and use ### or """ to separate the instruction and context
-        ("user", "Text: ###\n{context}\n###")]
+        ("user", "Logs: ###\n{context}\n###")]
 
     return messages
