1f7e4b92bf8683a7a351f8e58ddcc6dd4ec9a221
Matthew Casperson <matthewcasperson@gmail.com>
2024-04-06 07:41:56 +1000

    macos does not have cpu only torch

--- tests/application/copilot_chat_test.py
+++ tests/application/copilot_chat_test.py
@@ -59,6 +59,7 @@ class CopilotChatTest(unittest.TestCase):
             print(
                 "The tests will fail because Azurite is not running. Run Azureite with: "
                 + "docker run -d -p 10000:10000 -p 10001:10001 -p 10002:10002 mcr.microsoft.com/azure-storage/azurite")
+            print(e)
             return
 
         try:
