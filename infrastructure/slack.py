cb0badde60f6f9841d841c2e4fdba4edad7630d6
Matthew Casperson <matthewcasperson@gmail.com>
2024-04-07 12:39:07 +1000

    Use an async library for sending the http request to slack

--- infrastructure/slack.py
+++ infrastructure/slack.py
@@ -1,24 +1,15 @@
 import json
 import traceback
-from threading import Thread
+
+import aiohttp
 
 from domain.logging.app_logging import configure_logging
 from domain.validation.argument_validation import ensure_string_not_empty
-from infrastructure.http_pool import http
 
 logger = configure_logging(__name__)
 
 
-def send_slack_message_async(message, slack_url):
-    """
-    Send a message to a slack channel asynchronously
-    :param message: The message to send
-    :param slack_url: The slack URL
-    """
-    Thread(target=send_slack_message, args=(message, slack_url)).start()
-
-
-def send_slack_message(message, slack_url):
+async def send_slack_message_async(message, slack_url):
     """
     Sends a message to a slack channel
     :param message: The message to send
@@ -29,10 +20,12 @@ def send_slack_message(message, slack_url):
 
     try:
         data = json.dumps({"text": message})
-        resp = http.request("POST", slack_url, headers={'Content-Type': 'application/json'}, body=data)
 
-        if resp.status != 200:
-            logger.error(resp.data)
+        async with aiohttp.ClientSession(headers={'Content-Type': 'application/json'}) as session:
+            async with session.post(slack_url, data=data) as response:
+                if response.status != 200:
+                    logger.error(response.text())
+
     except Exception as e:
         error_message = getattr(e, 'message', repr(e))
         logger.error(error_message)
