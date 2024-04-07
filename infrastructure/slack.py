09f75bb0f80f12e2879a95f481f3fd3d892f4e85
Matthew Casperson <matthewcasperson@gmail.com>
2024-04-07 13:38:38 +1000

    Don't use async

--- infrastructure/slack.py
+++ infrastructure/slack.py
@@ -1,15 +1,14 @@
 import json
 import traceback
 
-import aiohttp
-
 from domain.logging.app_logging import configure_logging
 from domain.validation.argument_validation import ensure_string_not_empty
+from infrastructure.http_pool import http
 
 logger = configure_logging(__name__)
 
 
-async def send_slack_message(message, slack_url):
+def send_slack_message(message, slack_url):
     """
     Sends a message to a slack channel
     :param message: The message to send
@@ -20,12 +19,10 @@ async def send_slack_message(message, slack_url):
 
     try:
         data = json.dumps({"text": message})
+        resp = http.request("POST", slack_url, headers={'Content-Type': 'application/json'}, body=data)
 
-        async with aiohttp.ClientSession(headers={'Content-Type': 'application/json'}) as session:
-            async with session.post(slack_url, data=data) as response:
-                if response.status != 200:
-                    logger.error(response.text())
-
+        if resp.status != 200:
+            logger.error(resp.data)
     except Exception as e:
         error_message = getattr(e, 'message', repr(e))
         logger.error(error_message)
