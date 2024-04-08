edfd922e88f984ed8bf519b0b51254ef603743d0
Matthew Casperson <matthewcasperson@gmail.com>
2024-04-08 10:33:26 +1000

    Increase the context as the timeouts looked like HTML in markdown

--- domain/config/openai.py
+++ domain/config/openai.py
@@ -2,8 +2,7 @@
 # generate incorrect responses.
 # Lists of simple resources usually started to fail around the 40 item mark.
 # Lists of complex resources, like deployments with release notes, started to fail around the 30 item mark.
-# Complex lists worked around the 20 mark, but timed out when requested through the Copilot interface.
-max_context = 10
+max_context = 20
 
 # "Adversarial" queries can tie up a LLM for a long time. The Copilot interface times out after 60 seconds.
 # Our timeout is set to 45 seconds to allow for some additional processing time.
