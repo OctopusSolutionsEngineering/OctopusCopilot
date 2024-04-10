ef1543a3e9009ce4a8625a3b7a8d5fb1c197dafd
Matthew Casperson <matthewcasperson@gmail.com>
2024-04-10 15:52:04 +1000

    Replace self.assertEquals with self.assertEqual

--- tests/domain/strip_markdown_urls_test.py
+++ tests/domain/strip_markdown_urls_test.py
@@ -5,12 +5,12 @@ from domain.sanitizers.url_remover import strip_markdown_urls
 
 class UrlValidation(unittest.TestCase):
     def test_url_removal(self):
-        self.assertEquals(None, strip_markdown_urls(None))
-        self.assertEquals(" ", strip_markdown_urls(" "))
-        self.assertEquals("", strip_markdown_urls(""))
-        self.assertEquals(1, strip_markdown_urls(1))
-        self.assertEquals([], strip_markdown_urls([]))
-        self.assertEquals({}, strip_markdown_urls({}))
-        self.assertEquals("https://google.com", strip_markdown_urls("[https://google.com](https://google.com)"))
-        self.assertEquals("http://google.com", strip_markdown_urls("[http://google.com](http://google.com)"))
-        self.assertEquals("whatever", strip_markdown_urls("[whatever](http://google.com)"))
+        self.assertEqual(None, strip_markdown_urls(None))
+        self.assertEqual(" ", strip_markdown_urls(" "))
+        self.assertEqual("", strip_markdown_urls(""))
+        self.assertEqual(1, strip_markdown_urls(1))
+        self.assertEqual([], strip_markdown_urls([]))
+        self.assertEqual({}, strip_markdown_urls({}))
+        self.assertEqual("https://google.com", strip_markdown_urls("[https://google.com](https://google.com)"))
+        self.assertEqual("http://google.com", strip_markdown_urls("[http://google.com](http://google.com)"))
+        self.assertEqual("whatever", strip_markdown_urls("[whatever](http://google.com)"))
