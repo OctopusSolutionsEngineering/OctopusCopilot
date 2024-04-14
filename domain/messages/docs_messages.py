b4d86544b2980911289bb1f08ff298869d5b440b
Matthew Casperson <matthewcasperson@gmail.com>
2024-04-14 14:10:44 +1000

    Reformatted the docs prompt

--- domain/messages/docs_messages.py
+++ domain/messages/docs_messages.py
@@ -7,5 +7,6 @@ def docs_prompt(context):
     :param context: The raw documentation context
     :return: The LLM messages
     """
-    return [('user', context[:max_chars].replace("{", "{{").replace("}", "}}")),
+    return [('system', "You must prioritise the details in the Documentation context when answering the question."),
+            ('user', 'Documentation: ###\n' + context[:max_chars].replace("{", "{{").replace("}", "}}\n###")),
             ('user', "{input}")]
