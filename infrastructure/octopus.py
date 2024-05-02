759e5b98b456d1c5bb47e54f7656431158d51377
Matthew Casperson <matthewcasperson@gmail.com>
2024-05-02 12:27:00 +1000

    Fix case where an environment is not defined

--- infrastructure/octopus.py
+++ infrastructure/octopus.py
@@ -659,7 +659,10 @@ def get_deployment_logs(space_name, project_name, environment_name, tenant_name,
         deployments = flatten_list(map(lambda r: r["Deployments"][environment['Id']], filtered_releases))
     else:
         # Every deployment is a candidate
-        deployments = flatten_list(map(lambda r: r["Deployments"][environment['Id']], releases["Releases"]))
+        # Get all the deployments belonging to a release
+        deployment_environments = list(map(lambda r: r["Deployments"], releases["Releases"]))
+        # Get all the deployments to all environments
+        deployments = flatten_list(map(lambda dep_env: flatten_list(map(lambda env: dep_env[env], dep_env.keys())), deployment_environments))
 
     if tenant:
         deployments = list(filter(lambda d: d["TenantId"] == tenant["Id"], deployments))
