31df026f169091406ee6cac7eca05a2692396b15
Matthew Casperson <matthewcasperson@gmail.com>
2024-06-05 09:36:01 +1000

    Updated help text

--- function_app.py
+++ function_app.py
@@ -697,7 +697,7 @@ Here are some sample queries you can ask:
 * @octopus-ai-app Summarize the deployment logs for the latest deployment for the project "{first_project}" in the "{first_environment}" environment in the space called "{space_name}"
 * @octopus-ai-app List any URLs printed in the deployment logs for the latest deployment for the project "{first_project}" in the "{first_environment}" environment in the space called "{space_name}"
 * @octopus-ai-app How do I enable server side apply?
-* @octopus-ai-app The status "Success" is represented with the 🟢 character. The status "In Progress" is represented by the 🔵 character. Other statuses are represented with the 🔴 character. Show the release version, release notes, and status of the last 5 deployments for the project "{first_project}" in the "{first_environment}" environment in the "{space_name}" space in a markdown table.
+* @octopus-ai-app The status "Success" is represented with the 🟢 character. The status "Executing" is represented by the 🔵 character. The status "In Progress" is represented by the ⚪ character. Other statuses are represented with the 🔴 character. Show the release version, release notes, and status of the last 5 deployments for the project "{first_project}" in the "{first_environment}" environment in the "{space_name}" space in a markdown table.
 
 See the [documentation](https://octopus.com/docs/administration/copilot) for more information.
 """)
