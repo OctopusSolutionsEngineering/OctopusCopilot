7dcf06b9b9380d8f163157a28a81631c0f813d0e
Matthew Casperson <matthewcasperson@gmail.com>
2024-06-07 08:23:06 +1000

    Remove s from duration

--- domain/date/date_difference.py
+++ domain/date/date_difference.py
@@ -1,8 +1,10 @@
 def get_date_difference_summary(difference):
     if difference.days > 0:
-        return f"{difference.days} days"
+        return f"{difference.days} day{'s' if difference.days > 1 else ''}"
     if difference.seconds > 3600:
-        return f"{difference.seconds // 3600} hours"
+        hours = difference.seconds // 3600
+        return f"{hours} hour{'s' if hours > 1 else ''}"
     if difference.seconds > 60:
-        return f"{difference.seconds // 60} minutes"
-    return f"{difference.seconds} seconds"
+        minutes = difference.seconds // 60
+        return f"{minutes} minute{'s' if minutes > 1 else ''}"
+    return f"{difference.seconds} second{'s' if difference.seconds > 1 else ''}"
--- tests/domain/get_date_difference_summary_test.py
+++ tests/domain/get_date_difference_summary_test.py
@@ -4,6 +4,14 @@ from domain.date.date_difference import get_date_difference_summary
 
 
 class DateDifferenceTest(unittest.TestCase):
+    def test_date_difference_day(self):
+        from datetime import datetime
+        date1 = datetime(2024, 1, 1)
+        date2 = datetime(2024, 1, 2)
+        difference = date2 - date1
+
+        self.assertEqual("1 day", get_date_difference_summary(difference))
+
     def test_date_difference_days(self):
         from datetime import datetime
         date1 = datetime(2024, 1, 1)
@@ -11,6 +19,14 @@ class DateDifferenceTest(unittest.TestCase):
         difference = date2 - date1
 
         self.assertEqual("30 days", get_date_difference_summary(difference))
+
+    def test_date_difference_hours(self):
+        from datetime import datetime
+        date1 = datetime(2024, 1, 1, 1, 1, 1)
+        date2 = datetime(2024, 1, 1, 2, 1, 1)
+        difference = date2 - date1
+
+        self.assertEqual("1 hour", get_date_difference_summary(difference))
 
     def test_date_difference_hours(self):
         from datetime import datetime
@@ -27,6 +43,14 @@ class DateDifferenceTest(unittest.TestCase):
         difference = date2 - date1
 
         self.assertEqual("2 minutes", get_date_difference_summary(difference))
+
+    def test_date_difference_second(self):
+        from datetime import datetime
+        date1 = datetime(2024, 1, 1, 1, 1, 1)
+        date2 = datetime(2024, 1, 1, 1, 1, 2)
+        difference = date2 - date1
+
+        self.assertEqual("1 second", get_date_difference_summary(difference))
 
     def test_date_difference_seconds(self):
         from datetime import datetime
