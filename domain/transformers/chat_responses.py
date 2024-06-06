18461d2e60f1ce80e4cf1329dbce1ae5f78ab25b
Matthew Casperson <matthewcasperson@gmail.com>
2024-06-06 13:34:18 +1000

    Display the tenant name

--- domain/transformers/chat_responses.py
+++ domain/transformers/chat_responses.py
@@ -104,7 +104,7 @@ def get_runbook_dashboard_response(dashboard, get_tenant):
             runs = dashboard["RunbookRuns"][environment]
             for run in runs:
                 if run['TenantId'] == tenant or (not run['TenantId'] and tenant == "Untenanted"):
-                    table += f"| {"Untenanted" if not run['TenantId'] else run['TenantId']} "
+                    table += f"| {"Untenanted" if not run['TenantId'] else get_tenant(run['TenantId'])} "
                     icon = get_state_icon(run['State'], run['HasWarningsOrErrors'])
                     table += f"| {icon} "
                     table += "|\n"
