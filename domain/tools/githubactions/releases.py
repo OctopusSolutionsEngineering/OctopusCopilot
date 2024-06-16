4a6c0ea21ed209a08b19a7b03c8571392ed4485d
Matthew Casperson <matthewcasperson@gmail.com>
2024-06-16 16:41:24 +1000

    Use a common debug building function

--- domain/tools/githubactions/releases.py
+++ domain/tools/githubactions/releases.py
@@ -5,9 +5,9 @@ from domain.context.octopus_context import collect_llm_context
 from domain.defaults.defaults import get_default_argument, get_default_argument_list
 from domain.performance.timing import timing_wrapper
 from domain.response.copilot_response import CopilotResponse
-from domain.sanitizers.sanitize_strings import to_lower_case_or_none
 from domain.sanitizers.sanitized_list import sanitize_environments, sanitize_tenants, sanitize_name_fuzzy, \
     sanitize_space, sanitize_names_fuzzy, sanitize_projects, get_item_or_none, update_query
+from domain.tools.debug import get_params_message
 from domain.transformers.deployments_from_dashboard import get_deployments_from_dashboard
 from domain.transformers.deployments_from_release import get_deployments_for_project
 from infrastructure.octopus import get_spaces_generator, get_space_id_and_name_from_name, get_projects_generator
@@ -52,6 +52,17 @@ def releases_query_callback(github_user, api_key, url, log_query):
     def releases_query_callback_implementation(original_query, messages, space, projects, environments, channels,
                                                releases, tenants, dates):
 
+        debug_text = get_params_message(github_user,
+                                        releases_query_callback_implementation.__name__,
+                                        original_query=original_query,
+                                        space=space,
+                                        projects=projects,
+                                        environments=environments,
+                                        channels=channels,
+                                        releases=releases,
+                                        tenants=tenants,
+                                        dates=dates)
+
         sanitized_environments = sanitize_environments(original_query, environments)
         sanitized_tenants = sanitize_tenants(tenants)
 
@@ -61,11 +72,11 @@ def releases_query_callback(github_user, api_key, url, log_query):
         space = get_default_argument(github_user,
                                      sanitized_space["matched"] if sanitized_space else None, "Space")
 
-        warnings = ""
+        warnings = []
 
         if not space:
             space = next(get_spaces_generator(api_key, url), {"Name": "Default"}).get("Name")
-            warnings = f"The query did not specify a space so the so the space named {space} was assumed."
+            warnings.append(f"The query did not specify a space so the so the space named {space} was assumed.")
 
         space_id, actual_space_name = get_space_id_and_name_from_name(space, api_key, url)
 
@@ -107,55 +118,43 @@ def releases_query_callback(github_user, api_key, url, log_query):
             # returned to supply the dashboard. The results are broad, but not deep.
             context["json"] = get_deployments_from_dashboard(space_id, api_key, url)
 
-        chat_response = collect_llm_context(processed_query,
-                                            messages,
-                                            context,
-                                            space_id,
-                                            query_project,
-                                            None,
-                                            None,
-                                            query_tenants,
-                                            None,
-                                            ["<all>"] if not query_environments else query_environments,
-                                            None,
-                                            None,
-                                            None,
-                                            None,
-                                            None,
-                                            None,
-                                            None,
-                                            None,
-                                            channels,
-                                            releases,
-                                            None,
-                                            None,
-                                            dates,
-                                            api_key,
-                                            url,
-                                            log_query)
+        chat_response = [collect_llm_context(processed_query,
+                                             messages,
+                                             context,
+                                             space_id,
+                                             query_project,
+                                             None,
+                                             None,
+                                             query_tenants,
+                                             None,
+                                             ["<all>"] if not query_environments else query_environments,
+                                             None,
+                                             None,
+                                             None,
+                                             None,
+                                             None,
+                                             None,
+                                             None,
+                                             None,
+                                             channels,
+                                             releases,
+                                             None,
+                                             None,
+                                             dates,
+                                             api_key,
+                                             url,
+                                             log_query)]
 
-        additional_information = ""
+        additional_information = []
         if not query_project:
-            additional_information = (
-                    "\nThe query did not specify a project so the response is limited to the latest deployments for all projects."
-                    + "\nTo see more detailed information, specify a project name in the query.")
+            additional_information.append(
+                "\nThe query did not specify a project so the response is limited to the latest deployments for all projects."
+                + "\nTo see more detailed information, specify a project name in the query.")
 
-        # Debug mode shows the entities extracted from the query
-        debug_text = ""
-        debug = get_default_argument(github_user, None, "Debug")
-        if to_lower_case_or_none(debug) == "true":
-            additional_information = (releases_query_callback_implementation.__name__
-                                      + " was called with the following parameters:"
-                                      + f"\n* Original Query: {original_query}"
-                                      + f"\n* Space: {space}"
-                                      + f"\n* Projects: {projects}"
-                                      + f"\n* Environments: {environments}"
-                                      + f"\n* Channels: {channels}"
-                                      + f"\n* Releases: {releases}"
-                                      + f"\n* Tenants: {tenants}"
-                                      + f"\n* Dates: {dates}")
+        chat_response.extend(warnings)
+        chat_response.extend(additional_information)
+        chat_response.extend(debug_text)
 
-        return CopilotResponse(
-            "\n\n".join(filter(lambda x: x, [chat_response, warnings, additional_information, debug_text])))
+        return CopilotResponse("\n\n".join(chat_response))
 
     return releases_query_callback_implementation
