1227ea3ae277e499729d2cf8a1acc5e78200d9af
Matthew Casperson <matthewcasperson@gmail.com>
2024-06-16 18:53:41 +1000

    Fixed test

--- tests/domain/get_params_message_test.py
+++ tests/domain/get_params_message_test.py
@@ -5,5 +5,7 @@ from domain.tools.debug import get_params
 
 class EnsureTests(unittest.TestCase):
     def test_get_params_message(self):
-        self.assertEqual(get_params("start", test="test"),
+        self.assertEqual(get_params(True, "start", test="test"),
                          ["start was called with the following parameters:\n* test: test"])
+        self.assertEqual(get_params(False, "start", test="test"),
+                         ["start was processed with the following parameters:\n* test: test"])
