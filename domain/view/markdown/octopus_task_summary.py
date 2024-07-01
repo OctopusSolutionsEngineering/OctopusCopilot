9434803ac583295b588af440f1b89b243e0aca49
Matthew Casperson <matthewcasperson@gmail.com>
2024-07-02 07:02:05 +1000

    Highlights are not recursive

--- domain/view/markdown/octopus_task_summary.py
+++ domain/view/markdown/octopus_task_summary.py
@@ -1,13 +1,6 @@
 from domain.view.markdown.markdown_icons import get_activity_log_state_icon
 
 
-def get_highlights(log_item):
-    filtered_logs = list(filter(lambda x: x["Category"] == "Highlight", log_item["LogElements"]))
-    for child in log_item["Children"]:
-        filtered_logs.extend(get_highlights(child))
-    return filtered_logs
-
-
 def get_summary(log_item, depth):
     if depth == 0 and len(log_item["Children"]) == 0:
         return f"No logs found (status: {log_item['Status']})."
@@ -16,7 +9,7 @@ def get_summary(log_item, depth):
 
     messages = [f"{'&ensp;' * depth}{icon} {log_item['Name']}"]
 
-    filtered_logs = get_highlights(log_item)
+    filtered_logs = list(filter(lambda x: x["Category"] == "Highlight", log_item["LogElements"]))
 
     messages.extend(list(map(lambda e: f"{'&ensp;' * (depth + 1)}{e['MessageText']}", filtered_logs)))
 
