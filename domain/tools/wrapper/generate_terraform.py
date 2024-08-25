52b15e7d74825d69e785244102614ba463fc774c
Matthew Casperson <matthewcasperson@gmail.com>
2024-08-26 07:03:18 +1000

    Added more sample prompts

--- domain/tools/wrapper/generate_terraform.py
+++ domain/tools/wrapper/generate_terraform.py
@@ -9,6 +9,11 @@ def generate_terraform_wrapper(query, callback, github_token, logging=None):
     def generate_terraform():
         """Generates the Terraform configuration that matches the query. Use this function when
         the query is asking to generate Terraform configuration, Terraform modules, or sample HCL.
+        Example prompts include:
+        * Create a sample terraform module with three environments called "Development", "Test", and "Production"
+        * Generate a Terraform module that creates a project called "My Project" with a runbook called "My Runbook"
+        * Create a Terraform configuration that creates a project called "My Project" with a runbook called "My Runbook" and a target called "My Target"
+        * Generate a Terraform configuration file that creates a project called "My Project" in the project group "My Project Group" with a runbook called "My Runbook" and a target called "My Target"
         """
 
         if logging:
