1964527531028bc76a51217772e8be471e6e8c54
Matthew Casperson <matthewcasperson@gmail.com>
2024-08-26 15:32:48 +1000

    Fixed linting

--- tests/application/copilot_chat_test.py
+++ tests/application/copilot_chat_test.py
@@ -196,24 +196,24 @@ class CopilotChatTest(unittest.TestCase):
     @retry((AssertionError, RateLimitError), tries=3, delay=2)
     def test_general_solution(self):
         prompt = minify_strings("""Suggest a solution for the following issue:
-        
+
         Hello Octopus Support,
 
         Today we discovered an interesting behavior, which does look like a bug, and would like to have some assistance on it.
-        
+
         With one of our new projects we started observing Helm step failures – Octopus is unable to find values from the chart. For example, we have this deployment (done today in the morning):
-        
+
         <URL>
-        
+
         This project was previously set to get additional values from file in the chart (we have used this approach multiple times before, and it still works for other projects like <URL>), but for this one we were getting this error:
-        
+
         Upon further investigation, I noticed two things:
-        
+
             Projects that are working have no Octopus.Action[helm-upgrade].Helm.TemplateValuesSources system variable, but the one that is failing – does have it.
             Projects that are working have this line in logs: Unable to find values files at path `values.PPE.yaml`. Chart package contains root directory with chart name, so looking for values in there.
-        
+
         So my current theory is that something changed in Helm step, and now there is no heuristic to find values like we had before, and thus new projects are failing (because working directory for the step is not the folder of the chart, but the staging area). I was able to get the project working by forcefully passing additional arguments (-f redpanda-wholesale-us\values.STAGING.yaml) and making it look directly in the chart folder, but that’s a workaround – we would prefer a proper fix from your side.
-        
+
         You can log in to our instance to check things, but please revert any changes you do.
         """)
         response = copilot_handler_internal(build_request(prompt))
