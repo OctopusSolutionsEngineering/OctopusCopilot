2d6538451d21c31a9902b2be3c7b1f5fa3ea02af
Matthew Casperson <matthewcasperson@gmail.com>
2024-08-27 12:32:37 +1000

    Tests that cancel a task use a new runbook with a long running script

--- tests/terraform/simple/space_population/tenant.tf
+++ tests/terraform/simple/space_population/tenant.tf
@@ -2,7 +2,7 @@ resource "octopusdeploy_tenant" "tenant_team_a" {
   name        = "Marketing"
   description = "Marketing tenant"
   tenant_tags = ["regions/us-east-1", "regions/us-east-2"]
-  depends_on  = [octopusdeploy_tag.tag_a, octopusdeploy_tag.tag_b]
+  depends_on = [octopusdeploy_tag.tag_a, octopusdeploy_tag.tag_b]
 
   project_environment {
     environments = [
@@ -11,5 +11,14 @@ resource "octopusdeploy_tenant" "tenant_team_a" {
       octopusdeploy_environment.environment_production.id
     ]
     project_id = octopusdeploy_project.project_project2.id
+  }
+
+  project_environment {
+    environments = [
+      octopusdeploy_environment.environment_test.id,
+      octopusdeploy_environment.environment_development.id,
+      octopusdeploy_environment.environment_production.id
+    ]
+    project_id = octopusdeploy_project.project_project5.id
   }
 }
