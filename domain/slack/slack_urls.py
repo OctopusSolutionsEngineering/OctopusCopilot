51c7a4520c8e8cc3515b35ab99f901e4111ed77d
Matthew Casperson <matthewcasperson@gmail.com>
2024-08-29 16:09:12 +1000

    Fixed the link

--- domain/slack/slack_urls.py
+++ domain/slack/slack_urls.py
@@ -4,7 +4,7 @@ import urllib
 
 def generate_slack_login(github_user):
     return (
-        f"Log in to Slack to include Slack messages in the context:\n\n[log in](https://slack.com/oauth/v2/authorize?"
+        f"[Log in to Slack to include Slack messages in the context](https://slack.com/oauth/v2/authorize?"
         + f"client_id={os.environ.get('SLACK_CLIENT_ID')}"
         + f"&redirect_url={urllib.parse.quote(os.environ.get('SLACK_CLIENT_REDIRECT', ''))}"
         # + f"&user_scope={urllib.parse.quote('search:read,channels:history,groups:history,mpim:history,im:history')}"
