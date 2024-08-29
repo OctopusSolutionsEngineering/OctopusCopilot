2c1230225d22a8bbba08386b53273ea06d47b940
Matthew Casperson <matthewcasperson@gmail.com>
2024-08-29 14:51:07 +1000

    Strip slack and github tokens from responses

--- domain/sanitizers/sanitize_logs.py
+++ domain/sanitizers/sanitize_logs.py
@@ -10,6 +10,7 @@ from domain.validation.argument_validation import ensure_string
 sensitive_vars = [
     "[Aa][Pp][Ii]-[A-Za-z0-9]+",
     "xoxe.[A-Za-z0-9-]+",
+    "ghp_[A-Za-z0-9-]+",
 ]
 # From https://github.com/adobe/stringlifier with some minor modifications to work with the latest
 # versions of numpy.
