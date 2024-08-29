5b49d4b076709576c1aed5cefb073c21766a4723
Matthew Casperson <matthewcasperson@gmail.com>
2024-08-29 10:26:15 +1000

    Fixed test args

--- tests/infrastructure/tools/build_test_tools.py
+++ tests/infrastructure/tools/build_test_tools.py
@@ -2,10 +2,20 @@ import os
 
 from domain.config.zendesk import get_zendesk_user, get_zendesk_token
 from domain.tools.githubactions.default_values import default_value_callbacks
-from domain.tools.githubactions.generate_terraform import generate_terraform_callback_wrapper
-from domain.tools.githubactions.suggest_solution import suggest_solution_callback_wrapper
-from domain.tools.wrapper.function_definition import FunctionDefinitions, FunctionDefinition
-from domain.tools.wrapper.general_query import answer_general_query_wrapper, AnswerGeneralQuery
+from domain.tools.githubactions.generate_terraform import (
+    generate_terraform_callback_wrapper,
+)
+from domain.tools.githubactions.suggest_solution import (
+    suggest_solution_callback_wrapper,
+)
+from domain.tools.wrapper.function_definition import (
+    FunctionDefinitions,
+    FunctionDefinition,
+)
+from domain.tools.wrapper.general_query import (
+    answer_general_query_wrapper,
+    AnswerGeneralQuery,
+)
 from domain.tools.wrapper.generate_terraform import generate_terraform_wrapper
 from domain.tools.wrapper.how_to import how_to_wrapper
 from domain.tools.wrapper.suggest_solution import suggest_solution_wrapper
@@ -20,21 +30,47 @@ def how_to_callback(query, keywords):
 
 
 def build_mock_test_tools(tool_query):
-    docs_functions = [FunctionDefinition(tool) for tool in how_to_wrapper(tool_query, how_to_callback, None)]
+    docs_functions = [
+        FunctionDefinition(tool)
+        for tool in how_to_wrapper(tool_query, how_to_callback, None)
+    ]
     # Functions related to the default values
-    set_default_value, remove_default_value, get_default_value, get_all_default_values = default_value_callbacks(
-        lambda: "1234567")
-    return FunctionDefinitions([
-        FunctionDefinition(answer_general_query_wrapper(tool_query, general_query_handler), AnswerGeneralQuery),
-        FunctionDefinition(set_default_value),
-        FunctionDefinition(get_default_value),
-        FunctionDefinition(get_all_default_values),
-        FunctionDefinition(remove_default_value),
-        FunctionDefinition(
-            suggest_solution_wrapper(tool_query, suggest_solution_callback_wrapper(os.environ["TEST_GH_USER"]),
-                                     os.environ["GH_TEST_TOKEN"],
-                                     get_zendesk_user(),
-                                     get_zendesk_token())),
-        FunctionDefinition(
-            generate_terraform_wrapper(tool_query, generate_terraform_callback_wrapper(), os.environ["GH_TEST_TOKEN"])),
-    ], fallback=FunctionDefinitions(docs_functions))
+    (
+        set_default_value,
+        remove_default_value,
+        get_default_value,
+        get_all_default_values,
+    ) = default_value_callbacks(lambda: "1234567")
+    return FunctionDefinitions(
+        [
+            FunctionDefinition(
+                answer_general_query_wrapper(tool_query, general_query_handler),
+                AnswerGeneralQuery,
+            ),
+            FunctionDefinition(set_default_value),
+            FunctionDefinition(get_default_value),
+            FunctionDefinition(get_all_default_values),
+            FunctionDefinition(remove_default_value),
+            FunctionDefinition(
+                suggest_solution_wrapper(
+                    tool_query,
+                    suggest_solution_callback_wrapper(os.environ["TEST_GH_USER"]),
+                    os.environ["TEST_GH_USER"],
+                    os.environ["GH_TEST_TOKEN"],
+                    get_zendesk_user(),
+                    get_zendesk_token(),
+                    "",
+                    os.environ.get("ENCRYPTION_PASSWORD"),
+                    os.environ.get("ENCRYPTION_SALT"),
+                )
+            ),
+            FunctionDefinition(
+                generate_terraform_wrapper(
+                    tool_query,
+                    generate_terraform_callback_wrapper(),
+                    os.environ["GH_TEST_TOKEN"],
+                )
+            ),
+        ],
+        fallback=FunctionDefinitions(docs_functions),
+    )
