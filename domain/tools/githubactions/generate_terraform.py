ba21693e5a41cb455d86687e00bc4d6828c9ed50
Matthew Casperson <matthewcasperson@gmail.com>
2024-08-31 07:18:35 +1000

    Added some constants instead of magic strings

--- domain/tools/githubactions/generate_terraform.py
+++ domain/tools/githubactions/generate_terraform.py
@@ -1,3 +1,4 @@
+from domain.config.openai import mini_model
 from domain.response.copilot_response import CopilotResponse
 from infrastructure.openai import llm_message_query
 
@@ -5,7 +6,7 @@ from infrastructure.openai import llm_message_query
 def generate_terraform_callback_wrapper():
     def generate_terraform_callback_implementation(query, messages):
         context = {"input": query}
-        chat_response = [llm_message_query(messages, context, deployment="gpt-4o-mini")]
+        chat_response = [llm_message_query(messages, context, deployment=mini_model)]
 
         return CopilotResponse("\n\n".join(chat_response))
 
