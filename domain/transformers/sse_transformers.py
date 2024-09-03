c0302cbfc16bdbc5e7722db55dd51f95faf1ecbc
Matthew Casperson <matthewcasperson@gmail.com>
2024-09-03 16:59:15 +1000

    Updated to new protocol: https://github.com/github-technology-partners/copilot-partners/blob/main/docs/confirmations.md

--- domain/transformers/sse_transformers.py
+++ domain/transformers/sse_transformers.py
@@ -1,33 +1,55 @@
 import json
+import re
 
 from domain.errors.error_handling import sanitize_message
 
 
-def convert_to_sse_response(result, prompt_title=None, prompt_message=None, prompt_id=None):
+def convert_to_sse_response(
+    result, prompt_title=None, prompt_message=None, prompt_id=None
+):
     """
     Converts a string to an SSE data only response. Optionally includes a confirmation prompt.
     :param result: The text to convert to an SSE stream
     :return: The SSE data only stream
     """
 
-    stop = "data: " + json.dumps({"choices": [{"index": 0, "delta": {}, "finish_reason": "stop"}]})
+    stop = "data: " + json.dumps(
+        {"choices": [{"index": 0, "delta": {}, "finish_reason": "stop"}]}
+    )
 
     if not result.strip():
         return stop + "\n\n"
 
     content = list(
-        map(lambda line: "data: " + json.dumps(
-            {"choices": [{"index": 0, "delta": {"content": sanitize_message(line) + "\n"}}]}),
-            result.strip().split("\n")))
+        map(
+            lambda line: "data: "
+            + json.dumps(
+                {
+                    "choices": [
+                        {
+                            "index": 0,
+                            "delta": {"content": sanitize_message(line) + "\n"},
+                        }
+                    ]
+                }
+            ),
+            result.strip().split("\n"),
+        )
+    )
 
     if prompt_title and prompt_message and prompt_id:
-        prompt_data = "data: " + json.dumps(
-            {"copilot_confirmation": {"type": "action",
-                                      "title": prompt_title,
-                                      "message": prompt_message,
-                                      "confirmation": {
-                                          "id": prompt_id
-                                      }}})
+        prompt_data = (
+            "event: copilot_confirmation\n"
+            + "data: "
+            + json.dumps(
+                {
+                    "type": "action",
+                    "title": prompt_title,
+                    "message": prompt_message,
+                    "confirmation": {"id": prompt_id},
+                }
+            )
+        )
         content.append(prompt_data)
 
     content.append(stop)
@@ -44,10 +66,19 @@ def convert_from_sse_response(sse_response):
     :return: The string representation of the SSE response.
     """
 
-    responses = map(lambda line: json.loads(line.replace("data: ", "")),
-                    filter(lambda line: line.strip(), sse_response.split("\n")))
-    content_responses = filter(lambda response: "content" in response["choices"][0]["delta"], responses)
-    return "\n".join(map(lambda line: line["choices"][0]["delta"]["content"].strip(), content_responses))
+    responses = map(
+        lambda line: json.loads(line.replace("data: ", "")),
+        filter(lambda line: line.strip(), sse_response.split("\n")),
+    )
+    content_responses = filter(
+        lambda response: "content" in response["choices"][0]["delta"], responses
+    )
+    return "\n".join(
+        map(
+            lambda line: line["choices"][0]["delta"]["content"].strip(),
+            content_responses,
+        )
+    )
 
 
 def get_confirmation_id(sse_response):
@@ -56,7 +87,16 @@ def get_confirmation_id(sse_response):
     :param sse_response: The SSE response to get the confirmation id from.
     :return: The confirmation id.
     """
-    responses = map(lambda line: json.loads(line.replace("data: ", "")),
-                    filter(lambda line: line.strip(), sse_response.split("\n")))
-    confirmation_responses = filter(lambda response: "copilot_confirmation" in response, responses)
-    return next(map(lambda response: response["copilot_confirmation"]["confirmation"]["id"], confirmation_responses))
+    responses = map(
+        lambda line: json.loads(re.sub("^data:\\s*", "", line)),
+        filter(lambda line: line.strip().startswith("data:"), sse_response.split("\n")),
+    )
+    confirmation_responses = filter(
+        lambda response: response.get("type") == "action", responses
+    )
+    return next(
+        map(
+            lambda response: response["confirmation"]["id"],
+            confirmation_responses,
+        )
+    )
