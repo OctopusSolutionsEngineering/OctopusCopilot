b507d66567acc9942fffb04ece050a826cd3a868
Matthew Casperson <matthewcasperson@gmail.com>
2024-09-16 11:01:56 +1000

    fixed messages

--- domain/transformers/text_to_context.py
+++ domain/transformers/text_to_context.py
@@ -3,19 +3,21 @@ def get_context_from_text_array(items, type):
         return []
 
     return [
-        get_context_from_string(context, type)
+        get_individual_context_from_string(context, type)
         for context in items
         if isinstance(context, str)
     ]
+
+
+def get_individual_context_from_string(item, type):
+    return (
+        "system",
+        type + ": ###\n" + item.replace("{", "{{").replace("}", "}}") + "\n###",
+    )
 
 
 def get_context_from_string(item, type):
     if not item:
         return []
 
-    return [
-        (
-            "system",
-            type + ": ###\n" + item.replace("{", "{{").replace("}", "}}") + "\n###",
-        )
-    ]
+    return [get_individual_context_from_string(item, type)]
