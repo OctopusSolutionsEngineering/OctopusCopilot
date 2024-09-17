ba08a89a9f7d8c901daedc5f3ead08d59e5785b0
Matthew Casperson <matthewcasperson@gmail.com>
2024-09-18 07:56:19 +1000

    Still not getting git diff summaries by default

--- domain/messages/describe_deployment.py
+++ domain/messages/describe_deployment.py
@@ -5,7 +5,7 @@ def build_deployment_overview_prompt(few_shot=None, context=None):
             """You are a helpful agent that understands git diffs, git commits, github issues, and Octopus deployment logs.
                 You are asked to provide information about a deployment.
                 All the supplied information relates to changes that contributed to a deployment.
-                If git diffs are provided, you must provide a summary of the code that changed in the diffs in the response.
+                If git diffs are provided, you must provide a summary of the changes made to individual files in the response.
                 If a list of committers are provided, you must list the committers in the response.
                 If details of issues are provided, you must provide a summary of the issues in the response.
                 You must provide a summary of the deployment logs in the response.
