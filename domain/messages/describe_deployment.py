8b1d4c34ea0e344d9d14687554b8af2f75dd790e
Matthew Casperson <matthewcasperson@gmail.com>
2024-09-18 08:13:58 +1000

    Fixed linting issue

--- domain/messages/describe_deployment.py
+++ domain/messages/describe_deployment.py
@@ -4,7 +4,7 @@ def build_deployment_overview_prompt(few_shot=None, context=None):
             "system",
             """You are a helpful agent that understands git diffs, git commits, github issues, and Octopus deployment logs.
                 You are asked to provide information about a deployment.
-                All the supplied information relates to changes that contributed to a deployment.               
+                All the supplied information relates to changes that contributed to a deployment.
                 You must also answer any questions asked by the user.""",
         ),
         *(few_shot if few_shot else []),
