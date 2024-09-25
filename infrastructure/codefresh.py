28ae4be81251ddb30958b26405675dae9159c3ee
Mark Harrison <mark.harrison@octopus.com>
2024-09-25 18:23:15 +0100

    Fix default path for graphql queries

--- infrastructure/codefresh.py
+++ infrastructure/codefresh.py
@@ -32,7 +32,7 @@ def handle_response(callback):
             raise CodefreshRequestFailed(f"Request failed with {e.args}")
 
 
-def get_query(query_name, query_path="infrastructure/queries"):
+def get_query(query_name, query_path="queries"):
     query_path = os.path.join(query_path, f'{query_name}.graphql')
     with open(query_path, 'r') as file:
         query_content = file.read()
