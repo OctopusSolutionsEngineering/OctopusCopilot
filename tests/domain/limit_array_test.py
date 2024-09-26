72c76df4ba1de33815bcef470174be690c42e17c
Matthew Casperson <matthewcasperson@gmail.com>
2024-09-26 13:52:21 +1000

    Added more tests

--- tests/domain/limit_array_test.py
+++ tests/domain/limit_array_test.py
@@ -11,3 +11,12 @@ class LimitArrayTest(unittest.TestCase):
         input = ["a", "b", "c", "d", "e"]
         result = limit_array_to_max_char_length(input, 3)
         self.assertTrue(result == ["a", "b", "c"], result)
+
+    def test_limit_array_to_max_char_2(self):
+        input = ["a", "b", "cd", "d", "e"]
+        result = limit_array_to_max_char_length(input, 3)
+        self.assertTrue(result == ["a", "b"], result)
+
+    def test_limit_array_to_max_char_length_none(self):
+        result = limit_array_to_max_char_length(None, 3)
+        self.assertIsNone(result, result)
