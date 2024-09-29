a55120c392ce01ffd5f3528244f4362bfc582c76
Matthew Casperson <matthewcasperson@gmail.com>
2024-09-30 09:09:32 +1000

    Fixed help

--- domain/tools/githubactions/provide_help.py
+++ domain/tools/githubactions/provide_help.py
@@ -51,7 +51,7 @@ def ghu_help(github_user, api_key, url, log_query):
 
             # Create a Project
             This prompt creates a new project from the template. Make sure you replace `MyProject` with the name of your project:
-            * `@octopus-ai-app Run the runbook "Create Project" in the project "Microservice Template" in the space "FunanceTech DevEx" with variables: Project Name=MyProject`
+            * `@octopus-ai-app Run the runbook "Create Project" in the project "Microservice Template" in the "Administration" environment in the space "FunanceTech DevEx" with variables: Project Name=MyProject`
 
             # Deploy a Project
             This prompt deploys your project to the "Development" environment. Make sure you replace `MyProject` with the name of your project:
