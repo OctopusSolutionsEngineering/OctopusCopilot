8aeb8cf5fa81c17ed2b04a64ab72a0494b8fca1b
Matthew Casperson <matthewcasperson@gmail.com>
2024-11-28 15:36:15 +1000

    Updated the sample terraform

--- tests/domain/test_get_terraform_messages.py
+++ tests/domain/test_get_terraform_messages.py
@@ -5,7 +5,9 @@ from domain.messages.generate_hcl import get_live_messages
 
 
 class GetTerraformExamplesTest(unittest.TestCase):
-    @unittest.skip("This is only used to generate new examples")
+    @unittest.skip(
+        "This is only used to generate new examples for the generate_hcl.py file"
+    )
     def test_get_live_messages(self):
         messages = get_live_messages(os.environ.get("GH_TEST_TOKEN"))
         print(messages)
