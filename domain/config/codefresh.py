86e511b058fe1491edbd8f4e3c7e3070d7b49284
Matthew Casperson <matthewcasperson@gmail.com>
2025-04-07 14:14:26 +1000

    Assume an empty env var also needs the default value

--- domain/config/codefresh.py
+++ domain/config/codefresh.py
@@ -6,4 +6,6 @@ def get_codefresh_url():
     returns the url for the codefresh API control plane
     :return: The codefresh api url
     """
-    return os.environ.get("CODEFRESH_URL", "https://g.codefresh.io")
+    default_value = "https://g.codefresh.io"
+    url = os.environ.get("CODEFRESH_URL", default_value)
+    return url if url else default_value
