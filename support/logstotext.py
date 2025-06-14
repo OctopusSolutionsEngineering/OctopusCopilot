8ac6a12e8eeaedfe86ec1faab606a048afbf53e1
Matthew Casperson <matthewcasperson@gmail.com>
2025-06-14 11:05:59 +1000

    Fixed default input

--- support/logstotext.py
+++ support/logstotext.py
@@ -7,11 +7,7 @@ import ast
 # Download log files from the storage account under a path like
 # resourceId=/SUBSCRIPTIONS/36083A1B-9710-4AAD-AB70-05E0EACC40FD/RESOURCEGROUPS/OCTOPUSAIAGENT/PROVIDERS/MICROSOFT.WEB/SITES/SPACEBUILDER/y=2025/m=06/d=14/h=00/m=00/PT1H.json
 
-if len(sys.argv) < 2:
-    print(f"Usage: {sys.argv[0]} <input_file> [output_file]")
-    sys.exit(1)
-
-input_file = sys.argv[1]
+input_file = sys.argv[1] if len(sys.argv) > 2 else "PT1H.json"
 output_file = sys.argv[2] if len(sys.argv) > 2 else "output.log"
 
 with open(input_file, "r") as infile, open(output_file, "w") as outfile:
