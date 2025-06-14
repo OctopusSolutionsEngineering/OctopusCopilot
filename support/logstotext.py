588e4349ed2389b95b23bf7bf92f86bf1a065685
Matthew Casperson <matthewcasperson@gmail.com>
2025-06-14 10:43:27 +1000

    Added a script that can parse log files saved to azure storage

---
+++ support/logstotext.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+import ast
+
+# Download log files from the storage account under a path like
+# resourceId=/SUBSCRIPTIONS/36083A1B-9710-4AAD-AB70-05E0EACC40FD/RESOURCEGROUPS/OCTOPUSAIAGENT/PROVIDERS/MICROSOFT.WEB/SITES/SPACEBUILDER/y=2025/m=06/d=14/h=00/m=00/PT1H.json
+
+if len(sys.argv) < 2:
+    print(f"Usage: {sys.argv[0]} <input_file> [output_file]")
+    sys.exit(1)
+
+input_file = sys.argv[1]
+output_file = sys.argv[2] if len(sys.argv) > 2 else "output.log"
+
+with open(input_file, "r") as infile, open(output_file, "w") as outfile:
+    for line in infile:
+        try:
+            data = ast.literal_eval(line)
+            message = data.get("properties", {}).get("message", "")
+            outfile.write(message + "\n")
+        except Exception as e:
+            print(e)
+            continue
