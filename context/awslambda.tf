fbd36af8503ae0b45a5427882e7be196d21f1060
Matthew Casperson <matthewcasperson@gmail.com>
2025-06-24 17:59:17 +1000

    Updated context

--- context/awslambda.tf
+++ context/awslambda.tf
@@ -330,15 +330,15 @@ resource "octopusdeploy_process_step" "process_step_aws_lambda_attempt_login" {
   properties            = {
       }
   execution_properties  = {
-        "Octopus.Action.AwsAccount.UseInstanceRole" = "False"
-        "Octopus.Action.AwsAccount.Variable" = "Project.AWS.Account"
-        "Octopus.Action.Aws.AssumeRole" = "False"
         "Octopus.Action.Script.Syntax" = "PowerShell"
-        "Octopus.Action.Aws.Region" = "#{Project.AWS.Region}"
-        "Octopus.Action.Script.ScriptBody" = "# Get the current AWS user. This will only succeed if the AWS account is valid.\n# If the AWS account is not valid, this step will fail. We can detect the failure and offer next steps.\naws sts get-caller-identity"
+        "Octopus.Action.AwsAccount.Variable" = "Project.AWS.Account"
         "Octopus.Action.RunOnServer" = "true"
+        "Octopus.Action.AwsAccount.UseInstanceRole" = "False"
         "OctopusUseBundledTooling" = "False"
+        "Octopus.Action.Aws.Region" = "#{Project.AWS.Region}"
         "Octopus.Action.Script.ScriptSource" = "Inline"
+        "Octopus.Action.Script.ScriptBody" = "# Get the current AWS user. This will only succeed if the AWS account is valid.\n# If the AWS account is not valid, this step will fail. We can detect the failure and offer next steps.\naws sts get-caller-identity"
+        "Octopus.Action.Aws.AssumeRole" = "False"
       }
 }
 
@@ -361,11 +361,11 @@ resource "octopusdeploy_process_step" "process_step_aws_lambda_validate_setup" {
   properties            = {
       }
   execution_properties  = {
-        "OctopusUseBundledTooling" = "False"
         "Octopus.Action.RunOnServer" = "true"
         "Octopus.Action.GitRepository.Source" = "External"
         "Octopus.Action.Script.ScriptFileName" = "octopus/aws/ValidateLambdaSetup.ps1"
         "Octopus.Action.Script.ScriptSource" = "GitRepository"
+        "OctopusUseBundledTooling" = "False"
       }
 }
 
@@ -388,11 +388,11 @@ resource "octopusdeploy_process_step" "process_step_aws_lambda_check_smtp_config
   properties            = {
       }
   execution_properties  = {
+        "Octopus.Action.RunOnServer" = "true"
+        "Octopus.Action.GitRepository.Source" = "External"
         "Octopus.Action.Script.ScriptFileName" = "octopus/CheckSMTPConfigured.ps1"
         "Octopus.Action.Script.ScriptSource" = "GitRepository"
         "OctopusUseBundledTooling" = "False"
-        "Octopus.Action.RunOnServer" = "true"
-        "Octopus.Action.GitRepository.Source" = "External"
       }
 }
 
@@ -414,10 +414,10 @@ resource "octopusdeploy_process_step" "process_step_aws_lambda_send_deployment_s
         "Octopus.Step.ConditionVariableExpression" = "#{unless Octopus.Deployment.Error}#{if Octopus.Action[Check SMTP configuration].Output.SmtpConfigured == \"True\"}true#{/if}#{/unless}"
       }
   execution_properties  = {
-        "Octopus.Action.RunOnServer" = "false"
-        "Octopus.Action.Email.Body" = "#{Octopus.Project.Name} release version #{Octopus.Release.Number} is deploying to #{Octopus.Environment.Name}"
         "Octopus.Action.Email.Subject" = "#{Octopus.Project.Name} is deploying to #{Octopus.Environment.Name}"
         "Octopus.Action.Email.To" = "#{Octopus.Deployment.CreatedBy.EmailAddress}"
+        "Octopus.Action.RunOnServer" = "false"
+        "Octopus.Action.Email.Body" = "#{Octopus.Project.Name} release version #{Octopus.Release.Number} is deploying to #{Octopus.Environment.Name}"
       }
 }
 
@@ -439,9 +439,9 @@ resource "octopusdeploy_process_step" "process_step_aws_lambda_approve_productio
         "Octopus.Step.ConditionVariableExpression" = "#{unless Octopus.Deployment.Error}#{if Octopus.Action[Validate setup].Output.AwsLambdaConfigured == \"True\"}true#{/if}#{/unless}"
       }
   execution_properties  = {
+        "Octopus.Action.Manual.BlockConcurrentDeployments" = "False"
         "Octopus.Action.Manual.Instructions" = "Do you approve the production deployment?"
         "Octopus.Action.RunOnServer" = "false"
-        "Octopus.Action.Manual.BlockConcurrentDeployments" = "False"
       }
 }
 
@@ -473,26 +473,26 @@ resource "octopusdeploy_process_step" "process_step_aws_lambda_upload_lambda" {
         "Octopus.Step.ConditionVariableExpression" = "#{unless Octopus.Deployment.Error}#{if Octopus.Action[Validate setup].Output.AwsLambdaConfigured == \"True\"}true#{/if}#{/unless}"
       }
   execution_properties  = {
+        "Octopus.Action.RunOnServer" = "true"
+        "Octopus.Action.AwsAccount.Variable" = "Project.AWS.Account"
+        "OctopusUseBundledTooling" = "False"
+        "Octopus.Action.Aws.S3.BucketName" = "#{Project.AWS.Lambda.S3.BucketName}"
+        "Octopus.Action.Aws.Region" = "#{Project.AWS.Region}"
         "Octopus.Action.Aws.S3.PackageOptions" = jsonencode({
+        "tags" = []
+        "autoFocus" = "true"
         "bucketKey" = "#{Project.AWS.Lambda.S3.FileName}"
+        "bucketKeyBehaviour" = "Custom"
+        "metadata" = []
         "bucketKeyPrefix" = ""
         "storageClass" = "STANDARD"
-        "metadata" = []
-        "autoFocus" = "true"
-        "bucketKeyBehaviour" = "Custom"
         "cannedAcl" = "private"
         "variableSubstitutionPatterns" = ""
         "structuredVariableSubstitutionPatterns" = ""
-        "tags" = []
                 })
-        "OctopusUseBundledTooling" = "False"
-        "Octopus.Action.AwsAccount.UseInstanceRole" = "False"
-        "Octopus.Action.Aws.S3.BucketName" = "#{Project.AWS.Lambda.S3.BucketName}"
-        "Octopus.Action.RunOnServer" = "true"
         "Octopus.Action.Aws.AssumeRole" = "False"
+        "Octopus.Action.AwsAccount.UseInstanceRole" = "False"
         "Octopus.Action.Aws.S3.TargetMode" = "EntirePackage"
-        "Octopus.Action.AwsAccount.Variable" = "Project.AWS.Account"
-        "Octopus.Action.Aws.Region" = "#{Project.AWS.Region}"
       }
 }
 
@@ -524,20 +524,21 @@ resource "octopusdeploy_process_step" "process_step_aws_lambda_deploy_lambda_sam
         "Octopus.Step.ConditionVariableExpression" = "#{unless Octopus.Deployment.Error}#{if Octopus.Action[Validate setup].Output.AwsLambdaConfigured == \"True\"}true#{/if}#{/unless}"
       }
   execution_properties  = {
-        "Octopus.Action.RunOnServer" = "true"
-        "Octopus.Action.Aws.WaitForCompletion" = "True"
-        "Octopus.Action.AwsAccount.Variable" = "Project.AWS.Account"
-        "Octopus.Action.EnabledFeatures" = "Octopus.Features.JsonConfigurationVariables"
-        "OctopusUseBundledTooling" = "False"
         "Octopus.Action.Aws.CloudFormationStackName" = "#{Octopus.Space.Name | Replace \"[^A-Za-z0-9]\" \"-\"}-OctopubProductsSAMLambda-#{Octopus.Deployment.Id | Replace -}-#{Octopus.Environment.Name}"
-        "Octopus.Action.Aws.CloudFormationTemplate" = "sam.jvm.yaml"
         "Octopus.Action.Aws.IamCapabilities" = jsonencode([
         "CAPABILITY_AUTO_EXPAND",
         "CAPABILITY_IAM",
         "CAPABILITY_NAMED_IAM",
         ])
-        "Octopus.Action.AwsAccount.UseInstanceRole" = "False"
+        "Octopus.Action.Aws.TemplateSource" = "Package"
+        "Octopus.Action.RunOnServer" = "true"
+        "Octopus.Action.Aws.AssumeRole" = "False"
+        "Octopus.Action.AwsAccount.Variable" = "Project.AWS.Account"
+        "Octopus.Action.EnabledFeatures" = "Octopus.Features.JsonConfigurationVariables"
+        "OctopusUseBundledTooling" = "False"
         "Octopus.Action.Package.JsonConfigurationVariablesTargets" = "sam.jvm.yaml"
+        "Octopus.Action.Aws.WaitForCompletion" = "True"
+        "Octopus.Action.Aws.CloudFormationTemplate" = "sam.jvm.yaml"
         "Octopus.Action.Aws.Region" = "#{Project.AWS.Region}"
         "Octopus.Action.Aws.CloudFormation.Tags" = jsonencode([
         {
@@ -549,32 +550,31 @@ resource "octopusdeploy_process_step" "process_step_aws_lambda_deploy_lambda_sam
         "value" = "#{Octopus.Step.Id}"
                 },
         {
-        "value" = "#{if Octopus.RunBookRun.Id}#{Octopus.RunBookRun.Id}#{/if}#{unless Octopus.RunBookRun.Id}none#{/unless}"
         "key" = "OctopusRunbookRunId"
+        "value" = "#{if Octopus.RunBookRun.Id}#{Octopus.RunBookRun.Id}#{/if}#{unless Octopus.RunBookRun.Id}none#{/unless}"
                 },
         {
         "key" = "OctopusDeploymentId"
         "value" = "#{if Octopus.Deployment.Id}#{Octopus.Deployment.Id}#{/if}#{unless Octopus.Deployment.Id}none#{/unless}"
                 },
         {
-        "key" = "OctopusProjectId"
         "value" = "#{Octopus.Project.Id}"
+        "key" = "OctopusProjectId"
                 },
         {
-        "value" = "#{Octopus.Environment.Id}"
         "key" = "OctopusEnvironmentId"
+        "value" = "#{Octopus.Environment.Id}"
                 },
         {
         "key" = "Environment"
         "value" = "#{Octopus.Environment.Name}"
                 },
         {
-        "value" = "#{Octopus.Project.Name}"
         "key" = "DeploymentProject"
+        "value" = "#{Octopus.Project.Name}"
                 },
         ])
-        "Octopus.Action.Aws.AssumeRole" = "False"
-        "Octopus.Action.Aws.TemplateSource" = "Package"
+        "Octopus.Action.AwsAccount.UseInstanceRole" = "False"
       }
 }
 
@@ -596,11 +596,11 @@ resource "octopusdeploy_process_step" "process_step_aws_lambda_print_message_whe
   properties            = {
       }
   execution_properties  = {
+        "Octopus.Action.Script.Syntax" = "PowerShell"
         "OctopusUseBundledTooling" = "False"
         "Octopus.Action.RunOnServer" = "true"
         "Octopus.Action.Script.ScriptBody" = "# The variable to check must be in the format\n# #{Octopus.Step[\u003cname of the step that deploys the lambda\u003e].Status.Code}\nif (\"#{Octopus.Step[Deploy AWS Lambda function].Status.Code}\" -ieq \"Skipped\") {\n  Write-Highlight \"To complete the deployment, you must have the necessary AWS Lambda configuration.\"\n  Write-Highlight \"See details of the [required configuration](https://library.octopus.com/step-templates/9b5ee984-bdd2-49f0-a78a-07e21e60da8a/actiontemplate-aws-deploy-lambda-function).\"\n}"
         "Octopus.Action.Script.ScriptSource" = "Inline"
-        "Octopus.Action.Script.Syntax" = "PowerShell"
       }
 }
 
@@ -826,11 +826,11 @@ resource "octopusdeploy_process_step" "process_step_aws_lambda_checkov_sam_stati
   properties            = {
       }
   execution_properties  = {
-        "Octopus.Action.GitRepository.Source" = "External"
         "Octopus.Action.Script.ScriptFileName" = "octopus/RunCheckovScan.ps1"
         "Octopus.Action.Script.ScriptSource" = "GitRepository"
         "OctopusUseBundledTooling" = "False"
         "Octopus.Action.RunOnServer" = "true"
+        "Octopus.Action.GitRepository.Source" = "External"
       }
 }
 
--- context/deploymentorchestration.tf
+++ context/deploymentorchestration.tf
@@ -205,16 +205,182 @@ data "octopusdeploy_feeds" "feed_octopus_server__built_in_" {
   }
 }
 
+data "octopusdeploy_feeds" "feed_octopus_server_releases__built_in_" {
+  feed_type    = "OctopusProject"
+  ids          = null
+  partial_name = "Octopus Server Releases"
+  skip         = 0
+  take         = 1
+  lifecycle {
+    postcondition {
+      error_message = "Failed to resolve a feed called \"Octopus Server Releases (built-in)\". This resource must exist in the space before this Terraform configuration is applied."
+      condition     = length(self.feeds) != 0
+    }
+  }
+}
+
+data "octopusdeploy_worker_pools" "workerpool_default_worker_pool" {
+  ids          = null
+  partial_name = "Default Worker Pool"
+  skip         = 0
+  take         = 1
+}
+
+data "octopusdeploy_worker_pools" "workerpool_hosted_windows" {
+  ids          = null
+  partial_name = "Hosted Windows"
+  skip         = 0
+  take         = 1
+}
+
+data "octopusdeploy_channels" "channel_child_project_default" {
+  ids          = []
+  partial_name = "Default"
+  skip         = 0
+  take         = 1
+}
+
+resource "octopusdeploy_process" "process_child_project" {
+  count      = "${length(data.octopusdeploy_projects.project_child_project.projects) != 0 ? 0 : 1}"
+  project_id = "${length(data.octopusdeploy_projects.project_child_project.projects) != 0 ? data.octopusdeploy_projects.project_child_project.projects[0].id : octopusdeploy_project.project_child_project[0].id}"
+  depends_on = []
+}
+
+resource "octopusdeploy_process_step" "process_step_child_project_run_a_script" {
+  count                 = "${length(data.octopusdeploy_projects.project_child_project.projects) != 0 ? 0 : 1}"
+  name                  = "Run a Script"
+  type                  = "Octopus.Script"
+  process_id            = "${length(data.octopusdeploy_projects.project_child_project.projects) != 0 ? null : octopusdeploy_process.process_child_project[0].id}"
+  channels              = null
+  condition             = "Success"
+  environments          = null
+  excluded_environments = null
+  package_requirement   = "LetOctopusDecide"
+  slug                  = "run-a-script"
+  start_trigger         = "StartAfterPrevious"
+  tenant_tags           = null
+  worker_pool_id        = "${length(data.octopusdeploy_worker_pools.workerpool_hosted_windows.worker_pools) != 0 ? data.octopusdeploy_worker_pools.workerpool_hosted_windows.worker_pools[0].id : data.octopusdeploy_worker_pools.workerpool_default_worker_pool.worker_pools[0].id}"
+  properties            = {
+      }
+  execution_properties  = {
+        "OctopusUseBundledTooling" = "False"
+        "Octopus.Action.RunOnServer" = "true"
+        "Octopus.Action.Script.ScriptSource" = "Inline"
+        "Octopus.Action.Script.Syntax" = "PowerShell"
+        "Octopus.Action.Script.ScriptBody" = "echo \"Hello world\""
+      }
+}
+
+resource "octopusdeploy_process_steps_order" "process_step_order_child_project" {
+  count      = "${length(data.octopusdeploy_projects.project_child_project.projects) != 0 ? 0 : 1}"
+  process_id = "${length(data.octopusdeploy_projects.project_child_project.projects) != 0 ? null : octopusdeploy_process.process_child_project[0].id}"
+  steps      = ["${length(data.octopusdeploy_projects.project_child_project.projects) != 0 ? null : octopusdeploy_process_step.process_step_child_project_run_a_script[0].id}"]
+}
+
+variable "project_child_project_name" {
+  type        = string
+  nullable    = false
+  sensitive   = false
+  description = "The name of the project exported from Child Project"
+  default     = "Child Project"
+}
+variable "project_child_project_description_prefix" {
+  type        = string
+  nullable    = false
+  sensitive   = false
+  description = "An optional prefix to add to the project description for the project Child Project"
+  default     = ""
+}
+variable "project_child_project_description_suffix" {
+  type        = string
+  nullable    = false
+  sensitive   = false
+  description = "An optional suffix to add to the project description for the project Child Project"
+  default     = ""
+}
+variable "project_child_project_description" {
+  type        = string
+  nullable    = false
+  sensitive   = false
+  description = "The description of the project exported from Child Project"
+  default     = ""
+}
+variable "project_child_project_tenanted" {
+  type        = string
+  nullable    = false
+  sensitive   = false
+  description = "The tenanted setting for the project Untenanted"
+  default     = "Untenanted"
+}
+data "octopusdeploy_projects" "project_child_project" {
+  ids          = null
+  partial_name = "${var.project_child_project_name}"
+  skip         = 0
+  take         = 1
+}
+resource "octopusdeploy_project" "project_child_project" {
+  count                                = "${length(data.octopusdeploy_projects.project_child_project.projects) != 0 ? 0 : 1}"
+  name                                 = "${var.project_child_project_name}"
+  auto_create_release                  = false
+  default_guided_failure_mode          = "EnvironmentDefault"
+  default_to_skip_if_already_installed = false
+  discrete_channel_release             = false
+  is_disabled                          = false
+  is_version_controlled                = false
+  lifecycle_id                         = "${data.octopusdeploy_lifecycles.lifecycle_default_lifecycle.lifecycles[0].id}"
+  project_group_id                     = "${data.octopusdeploy_project_groups.project_group_default_project_group.project_groups[0].id}"
+  included_library_variable_sets       = []
+  tenanted_deployment_participation    = "${var.project_child_project_tenanted}"
+
+  connectivity_policy {
+    allow_deployments_to_no_targets = true
+    exclude_unhealthy_targets       = false
+    skip_machine_behavior           = "None"
+  }
+
+  versioning_strategy {
+    template = "#{Octopus.Version.LastMajor}.#{Octopus.Version.LastMinor}.#{Octopus.Version.NextPatch}"
+  }
+  description = "${var.project_child_project_description_prefix}${var.project_child_project_description}${var.project_child_project_description_suffix}"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
 resource "octopusdeploy_process" "process_deployment_orchestration" {
   count      = "${length(data.octopusdeploy_projects.project_deployment_orchestration.projects) != 0 ? 0 : 1}"
   project_id = "${length(data.octopusdeploy_projects.project_deployment_orchestration.projects) != 0 ? data.octopusdeploy_projects.project_deployment_orchestration.projects[0].id : octopusdeploy_project.project_deployment_orchestration[0].id}"
   depends_on = []
 }
 
+resource "octopusdeploy_process_step" "process_step_deployment_orchestration_deploy_a_release" {
+  count                 = "${length(data.octopusdeploy_projects.project_deployment_orchestration.projects) != 0 ? 0 : 1}"
+  name                  = "Deploy a Release"
+  type                  = "Octopus.DeployRelease"
+  process_id            = "${length(data.octopusdeploy_projects.project_deployment_orchestration.projects) != 0 ? null : octopusdeploy_process.process_deployment_orchestration[0].id}"
+  channels              = null
+  condition             = "Success"
+  environments          = null
+  excluded_environments = null
+  notes                 = "This step deploys a release of another project."
+  package_requirement   = "LetOctopusDecide"
+  primary_package       = { acquisition_location = "NotAcquired", feed_id = "${data.octopusdeploy_feeds.feed_octopus_server_releases__built_in_.feeds[0].id}", id = null, package_id = "${length(data.octopusdeploy_projects.project_child_project.projects) != 0 ? data.octopusdeploy_projects.project_child_project.projects[0].id : octopusdeploy_project.project_child_project[0].id}", properties = null }
+  slug                  = "deploy-a-release"
+  start_trigger         = "StartAfterPrevious"
+  tenant_tags           = null
+  properties            = {
+      }
+  execution_properties  = {
+        "Octopus.Action.RunOnServer" = "true"
+        "Octopus.Action.DeployRelease.DeploymentCondition" = "Always"
+        "Octopus.Action.DeployRelease.ProjectId" = "${length(data.octopusdeploy_projects.project_child_project.projects) != 0 ? data.octopusdeploy_projects.project_child_project.projects[0].id : octopusdeploy_project.project_child_project[0].id}"
+      }
+}
+
 resource "octopusdeploy_process_steps_order" "process_step_order_deployment_orchestration" {
   count      = "${length(data.octopusdeploy_projects.project_deployment_orchestration.projects) != 0 ? 0 : 1}"
   process_id = "${length(data.octopusdeploy_projects.project_deployment_orchestration.projects) != 0 ? null : octopusdeploy_process.process_deployment_orchestration[0].id}"
-  steps      = []
+  steps      = ["${length(data.octopusdeploy_projects.project_deployment_orchestration.projects) != 0 ? null : octopusdeploy_process_step.process_step_deployment_orchestration_deploy_a_release[0].id}"]
 }
 
 variable "project_deployment_orchestration_name" {
