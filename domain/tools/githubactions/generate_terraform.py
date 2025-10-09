12444701acdc3f1a93d1000b9c5543ca0760dcc9
Matthew Casperson <matthewcasperson@gmail.com>
2025-10-10 05:58:27 +1000

    Fixed model

--- domain/tools/githubactions/generate_terraform.py
+++ domain/tools/githubactions/generate_terraform.py
@@ -6,9 +6,7 @@ def generate_terraform_callback_wrapper():
     def generate_terraform_callback_implementation(query, messages):
         context = {"input": query}
         chat_response = [
-            llm_message_query(
-                messages, context, deployment="gpt-4-1-mini-terraform-finetuned"
-            )
+            llm_message_query(messages, context, deployment="gpt-4.1-mini")
         ]
 
         # The LLM has been finetuned to respond only with the terraform. We wrap it up in a code block
