4efa39a85b9cf14a6c5eff6a4063f1c38846a750
Matthew Casperson <matthewcasperson@gmail.com>
2025-11-19 09:07:02 +1000

    Update mandatory step name in deployment process validation

--- tests/application/test_copilot_chat_create_projects.py
+++ tests/application/test_copilot_chat_create_projects.py
@@ -636,7 +636,7 @@ class CopilotChatTestCreateProjects(unittest.TestCase):
             number_of_steps > 2,
             f"The deployment process should have at least two steps. It has: {number_of_steps}",
         )
-        mandatory_step = "Validate setup"
+        mandatory_step = "Approve Production Deployment"
         self.assertTrue(
             any(step["Name"] == mandatory_step for step in deployment_process["Steps"]),
             f'The deployment process should have a step called "{mandatory_step}".',
