e8c8ddebcedc78c82847799ab1511b4b2cbefc0d
Matthew Casperson <matthewcasperson@gmail.com>
2025-11-27 06:32:52 +1000

    Format error message for unsupported resource creation for better readability

--- domain/tools/githubactions/projects/unsupported_resource.py
+++ domain/tools/githubactions/projects/unsupported_resource.py
@@ -4,5 +4,8 @@ from domain.response.copilot_response import CopilotResponse
 def unsupported_resource(type="", *args, **kwargs):
     def inner_unsupported_resource():
         # This is just a passthrough to indicate unsupported resource creation
-        return CopilotResponse(f"Creating this kind of resource ({type}) is not yet supported.")
+        return CopilotResponse(
+            f"Creating this kind of resource ({type}) is not yet supported."
+        )
+
     return inner_unsupported_resource
