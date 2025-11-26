355963224112a73b335ebe9e950f25e9667332d3
Matthew Casperson <matthewcasperson@gmail.com>
2025-11-27 07:04:38 +1000

    Update inner function signature in unsupported_resource for argument handling

--- domain/tools/githubactions/projects/unsupported_resource.py
+++ domain/tools/githubactions/projects/unsupported_resource.py
@@ -2,7 +2,7 @@ from domain.response.copilot_response import CopilotResponse
 
 
 def unsupported_resource(type="", *args, **kwargs):
-    def inner_unsupported_resource():
+    def inner_unsupported_resource(*args, **kwargs):
         # This is just a passthrough to indicate unsupported resource creation
         return CopilotResponse(
             f"Creating this kind of resource ({type}) is not yet supported."
