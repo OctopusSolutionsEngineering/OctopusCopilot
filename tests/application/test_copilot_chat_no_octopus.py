0f57afcde8bbbee6567483a41098c9f752a51625
Matthew Casperson <matthewcasperson@gmail.com>
2026-03-02 04:49:57 +1000

    Skip flaky test and improve prompt formatting in test_general_solution4

--- tests/application/test_copilot_chat_no_octopus.py
+++ tests/application/test_copilot_chat_no_octopus.py
@@ -175,10 +175,12 @@ From your diagram, the hosting company Octopus instance (UL-Octopus) is where de
             "approval" in response_text.casefold(), "Response was " + response_text
         )
 
+    @unittest.skip("Flaky test")
     @retry(RateLimitError, tries=3, delay=2)
     def test_general_solution4(self):
         prompt = minify_strings(
-            """Suggest a solution for the following issue: I have multiple tentacles shared between spaces. Are these tentacles counted as 1 item in the license, or does each tentacle count multiple times under the license?
+            """Suggest a solution for the following issue: I have multiple tentacles shared between spaces. 
+            Are these tentacles counted as 1 item in the license, or does each tentacle count multiple times under the license?
             """
         )
         response = copilot_handler_internal(build_no_octopus_request(prompt))
