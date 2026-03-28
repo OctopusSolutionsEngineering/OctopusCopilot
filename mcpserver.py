61f2cbffd0b4db7ff79c3da70ea5bd8967fc8d4b
Matthew Casperson <matthewcasperson@gmail.com>
2026-03-28 18:43:37 +1000

    Added example function

--- mcpserver.py
+++ mcpserver.py
@@ -3,5 +3,9 @@ from mcp.server import FastMCP
 # Create an MCP server
 mcp = FastMCP("AI Assistant", json_response=True)
 
+my_tool_function = lambda x: f"Hello, {x}!"
+
+mcp.add_tool(my_tool_function)
+
 if __name__ == "__main__":
     mcp.run(transport="stdio")
