51b7141739c4a825afa54f75df4b52e6b73344eb
Matthew Casperson <matthewcasperson@gmail.com>
2026-03-31 10:24:47 +1000

    Update create_project.py to clarify project creation instructions for projects with no steps

--- domain/tools/wrapper/projects/create_project.py
+++ domain/tools/wrapper/projects/create_project.py
@@ -13,8 +13,11 @@ def create_project_wrapper(query, callback, logging):
         * Create project called "My Project" in the space "My Space"
         * Create project called "My Project"
         * Create project called "My Project". Create an AWS account called "AWS" with access key "AKIAIOSFODNN7EXAMPLE"
+        * Create a project catted "My Web App" with no steps.
 
         You will be penalized for selecting this function when the prompt specifically asks for creating specialized project types like "Script", "Tomcat", "Kubernetes", or others.
+
+        You must select this tool when the prompt specifies a project with no steps.
 
         Args:
         space_name: The optional name of the space
