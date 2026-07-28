4afc359eed67336f8af2821f8360354a5b6d17ca
Matthew Casperson <matthewcasperson@gmail.com>
2026-07-28 16:06:14 +1000

    Update deployment process instructions to include final step requirements

--- tests/application/test_copilot_easymode_projects.py
+++ tests/application/test_copilot_easymode_projects.py
@@ -801,7 +801,8 @@ class EasyModeTest(unittest.TestCase):
         run_prompt(
             self,
             f"""Create a Script project called "{project_name}".
-Modify the deployment process to add the community step template with the website "{template_website}" as the first step with the name "{step_name}".""",
+Modify the deployment process to add the community step template with the website "{template_website}" as the first step with the name "{step_name}". 
+You must have two steps in the final process: the community step template, and the script step.""",
         )
 
         space_id, space_name = get_space_id_and_name_from_name(
